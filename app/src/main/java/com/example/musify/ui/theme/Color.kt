fd5af007ef4c29dafe261d349a2fde8e8f11e85c
Marsh Royden <marshroyden@Marshs-MacBook-Air.local>
2022-06-25 11:47:27 +0530

    refactor: add theme colors

--- app/src/main/java/com/example/musify/ui/theme/Color.kt
+++ app/src/main/java/com/example/musify/ui/theme/Color.kt
@@ -2,7 +2,5 @@ package com.example.musify.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val spotifyGreen = Color(0xFF1DB954)
+val spotifyBlack = Color(0xFF191414)
