a4ce116e9977e66019b49746598591cf2f53c86e
Marsh Royden <marshroyden@Marshs-MacBook-Air.local>
2022-06-25 12:35:09 +0530

    refactor: remove 'spotifyBlack' and use the default black color

--- app/src/main/java/com/example/musify/ui/theme/Color.kt
+++ app/src/main/java/com/example/musify/ui/theme/Color.kt
@@ -3,4 +3,3 @@ package com.example.musify.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val spotifyGreen = Color(0xFF1DB954)
-val spotifyBlack = Color(0xFF191414)
--- app/src/main/java/com/example/musify/ui/theme/Theme.kt
+++ app/src/main/java/com/example/musify/ui/theme/Theme.kt
@@ -4,10 +4,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.runtime.Composable
 
-private val DarkColorPalette = darkColors(
-    primary = spotifyGreen,
-    background = spotifyBlack
-)
+private val DarkColorPalette = darkColors(primary = spotifyGreen)
 
 @Composable
 fun MusifyTheme(content: @Composable () -> Unit) {
