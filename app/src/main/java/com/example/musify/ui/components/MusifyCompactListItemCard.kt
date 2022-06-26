5f2541a611750d9093db565efff066372aceb0b1
Marsh Royden <marshroyden@Marshs-MacBook-Air.local>
2022-06-26 13:13:20 +0530

    refactor(MusifyCompactListItemCard): add overload with 'cardType' param

--- app/src/main/java/com/example/musify/ui/components/MusifyCompactListItemCard.kt
+++ app/src/main/java/com/example/musify/ui/components/MusifyCompactListItemCard.kt
@@ -2,18 +2,30 @@ package com.example.musify.ui.components
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import com.example.musify.R
 
 /**
- * A composable that represents a compact list item.
+ * An enum containing the all the types of list item cards.
+ */
+enum class ListItemCardType { ALBUM, ARTIST, SONG, PLAYLIST }
+
+/**
+ * A composable that represents a compact list item. This composable
+ * contain an argument for setting the [trailingButtonIcon]. To
+ * automatically set the trailing icon based on the [ListItemCardType]
+ * use the other overload.
  *
  * @param thumbnailImageUrlString the url of the image to use as the
  * thumbnail.
@@ -78,4 +90,40 @@ fun MusifyCompactListItemCard(
             }
         }
     }
+}
+
+/**
+ * A composable that represents a compact list item. This overload will
+ * ensure the use of correct trailing icon based on the [cardType].
+ * If a specific trailing icon is needed, use the other overload.
+ *
+ * @param thumbnailImageUrlString the url of the image to use as the
+ * thumbnail.
+ * @param title the title of the card.
+ * @param subtitle the subtitle of the card.
+ * @param onClick the callback to execute when the card is clicked
+ * @param onTrailingButtonIconClick the callback to execute when the trailingButtonIcon
+ * is clicked.
+ */
+@ExperimentalMaterialApi
+@Composable
+fun MusifyCompactListItemCard(
+    cardType: ListItemCardType,
+    thumbnailImageUrlString: String,
+    title: String,
+    subtitle: String,
+    onClick: () -> Unit,
+    onTrailingButtonIconClick: () -> Unit,
+) {
+    MusifyCompactListItemCard(
+        thumbnailImageUrlString = thumbnailImageUrlString,
+        title = title,
+        subtitle = subtitle,
+        onClick = onClick,
+        trailingButtonIcon = when (cardType) {
+            ListItemCardType.SONG -> Icons.Filled.MoreVert
+            else -> ImageVector.vectorResource(id = R.drawable.ic_baseline_chevron_right_24)
+        },
+        onTrailingButtonIconClick = onTrailingButtonIconClick
+    )
 }
