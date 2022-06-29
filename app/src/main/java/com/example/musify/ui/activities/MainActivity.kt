a4a0a71eee22784dadcda15c82226e945dfd9a8b
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-06-29 12:32:45 +0530

    refactor(MainActivity): wrap content with 'ProvideWindowInsets composable

--- app/src/main/java/com/example/musify/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/musify/ui/activities/MainActivity.kt
@@ -11,15 +11,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.musify.ui.theme.MusifyTheme
+import com.google.accompanist.insets.ProvideWindowInsets
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MusifyTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                ProvideWindowInsets {
+                    Surface(
+                        modifier = Modifier.fillMaxSize(),
+                        color = MaterialTheme.colors.background
+                    ) {
+                        Greeting("Android")
+                    }
                 }
             }
         }
