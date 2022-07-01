bc3d4ca24cf352c49969f6840af394d7d337237f
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-07-01 12:59:15 +0530

    refactor: define TrackListDTO and TrackDTO

---
+++ app/src/main/java/com/example/musify/data/dto/TrackListDTO.kt
@@ -0,0 +1,9 @@
+package com.example.musify.data.dto
+
+import com.google.gson.annotations.SerializedName
+
+/**
+ * A DTO object that contains a list of tracks.
+ */
+@JvmInline
+value class TrackListDTO(@SerializedName("tracks") val value: List<TrackDTO>)
