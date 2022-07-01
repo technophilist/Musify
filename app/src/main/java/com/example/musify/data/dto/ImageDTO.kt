ade4865099b9aca415c8ac684ceeccb359d2d4a0
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-07-01 11:10:09 +0530

    refactor: create Artist and Image DTO objects

---
+++ app/src/main/java/com/example/musify/data/dto/ImageDTO.kt
@@ -0,0 +1,11 @@
+package com.example.musify.data.dto
+
+/**
+ * A DTO object the contains information about an image's [height],
+ * [width] and [url].
+ */
+data class ImageDTO(
+    val height: Int,
+    val width: Int,
+    val url: String
+)
