9b862fdeb7158d2fc7b964820c88f951bd6d7582
Marsh Royden <marshroyden@Marshs-MacBook-Air.local>
2022-07-01 12:59:15 +0530

    refactor: define TrackListDTO and TrackDTO

---
+++ app/src/main/java/com/example/musify/data/dto/TrackDTO.kt
@@ -0,0 +1,14 @@
+package com.example.musify.data.dto
+
+/**
+ * A DTO object that contains information about a specific track.
+ */
+data class TrackDTO(
+    val id: String,
+    val name: String,
+    val preview_url: String?,
+    val is_playable: Boolean,
+    val explicit: Boolean,
+    val duration_ms: Int,
+    val album: AlbumDTO
+)
