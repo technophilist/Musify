9f897d236b592c66b0b525941491cadc5d743fee
Marsh Royden <marshroyden@Marshs-MacBook-Air.local>
2022-07-02 16:43:37 +0530

    refactor(TracksWithAlbumMetadataList): change from value class to data class

--- app/src/main/java/com/example/musify/data/dto/TracksWithAlbumMetadataList.kt
+++ app/src/main/java/com/example/musify/data/dto/TracksWithAlbumMetadataList.kt
@@ -5,5 +5,4 @@ import com.google.gson.annotations.SerializedName
 /**
  * A DTO object that contains a list of [TrackDTOWithAlbumMetadata].
  */
-@JvmInline
-value class TracksWithAlbumMetadataList(@SerializedName("tracks") val value: List<TrackDTOWithAlbumMetadata>)
+data class TracksWithAlbumMetadataList(@SerializedName("tracks") val value: List<TrackDTOWithAlbumMetadata>)
