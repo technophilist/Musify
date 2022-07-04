1518f00cca5e1b7e01c06bf15fe7ca1aa6c4e4df
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-07-04 12:49:46 +0530

    refactor(SearchResultsDTO): make properties nullable

--- app/src/main/java/com/example/musify/data/dto/SearchResultsDTO.kt
+++ app/src/main/java/com/example/musify/data/dto/SearchResultsDTO.kt
@@ -4,12 +4,16 @@ import com.fasterxml.jackson.annotation.JsonProperty
 
 /**
  * A DTO that contains the results of a search operation.
+ * All the properties are nullable because a search operation
+ * for just [tracks],[albums],[artists] or [playlists], or
+ * a combination of any of the above can be made, in which
+ * case, the other properties will be null.
  */
 data class SearchResultsDTO(
-    val tracks: Tracks,
-    val albums: Albums,
-    val artists: Artists,
-    val playlists: Playlists
+    val tracks: Tracks?,
+    val albums: Albums?,
+    val artists: Artists?,
+    val playlists: Playlists?
 ) {
     data class Tracks(@JsonProperty("items") val value: List<TrackDTOWithAlbumMetadata>)
     data class Albums(@JsonProperty("items") val value: List<AlbumMetadataDTO>)
