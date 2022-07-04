a22306878db024e4711e80006ec59db6f5da1ca0
Marsh Royden <marshroyden@Marshs-MacBook-Air.local>
2022-07-04 13:47:18 +0530

    refactor: define DefaultMusifyJacksonConverterFactory property

---
+++ app/src/main/java/com/example/musify/utils/DefaultMusifyJacksonConverterFactory.kt
@@ -0,0 +1,13 @@
+package com.example.musify.utils
+
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import retrofit2.converter.jackson.JacksonConverterFactory
+
+/**
+ * A [JacksonConverterFactory] that is configured to not throw an
+ * exception when an unknown json property is encountered.
+ */
+val DefaultMusifyJacksonConverterFactory: JacksonConverterFactory = JacksonConverterFactory.create(
+    jacksonObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+)
