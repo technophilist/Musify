900790205da117fde6da8a943d9693cb04db37fb
Marsh Royden <marshroyden@Marshs-MacBook-Air.local>
2022-07-05 16:20:22 +0530

    refactor: rename SpotifyTokenManager -> TokenManager

--- app/src/main/java/com/example/musify/data/remote/token/tokenmanager/TokenManager.kt
+++ app/src/main/java/com/example/musify/data/remote/token/tokenmanager/TokenManager.kt
@@ -8,11 +8,11 @@ import retrofit2.http.FormUrlEncoded
 import retrofit2.http.Header
 import retrofit2.http.POST
 
-interface SpotifyTokenManager : TokenManager<Response<AccessTokenResponseDTO>> {
+interface TokenManager {
 
     @FormUrlEncoded
     @POST(SpotifyEndPoints.ApiTokenEndPoint)
-    override suspend fun getAccessToken(
+     suspend fun getAccessToken(
         @Field("grant_type") grantType: String,
         @Header("Authorization") secret: String
     ): Response<AccessTokenResponseDTO>
