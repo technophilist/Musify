b8fb935ba85e29153b21a8aa2765352ffdbee503
Marsh Royden <marshroyden@Marshs-MacBook-Air.local>
2022-07-06 16:51:27 +0530

    test(SpotifyServiceTest): update test to accommodate for removal of Response return type

--- app/src/test/java/com/example/musify/data/remote/musicservice/SpotifyServiceTest.kt
+++ app/src/test/java/com/example/musify/data/remote/musicservice/SpotifyServiceTest.kt
@@ -19,7 +19,7 @@ class SpotifyServiceTest {
     @Before
     fun setup() {
         val oAuthToken =
-            "BQAMfnRBpJ8H6x07NCUs0cE5EkYFM6gL8ly407kO-4E9rg8XK_hP0L8lPGKrCiTl7Fu2-L-eW0HNZph7QJtyIHL_OTONuCMdr6YZQ31QA5eosTdXMh7lWvzsMYvuJldtuVYxhOuHq5vms8ZaK-7c9h_7SbhNQSuMZn0M3kgn9cH0zAB51pHSBlpfgUJo1H4khbw"
+            "BQBnbeDCSUOwr8_zJRAoAP0PZUB1uAKJElbINCuAoRKb2zGrb3ldFxvu_0fwSWVeSZMjJrSfWDe64W6kLGjvjvjSUxXFZUQdE-mhSJlEEfYHmtKcKJI"
         val client = OkHttpClient.Builder()
             .addInterceptor(Interceptor { interceptorChain ->
                 val request = interceptorChain.request()
@@ -41,92 +41,67 @@ class SpotifyServiceTest {
     fun getArtistInfoTest_validArtistId_returnsValidArtistDTO() = runBlocking {
         // given a valid artistId
         val artistId = validArtistId
-        // when fetching the artist
-        val response = musicService.getArtistInfoWithId(artistId)
-        // the body of the response shouldn't be null
-        assert(response.body() != null)
-        // the id of the fetched id must match with the 'artistId'
-        assert(response.body()!!.id == artistId)
+        // the artist must be fetched successfully
+        val fetchedArtist = musicService.getArtistInfoWithId(artistId)
+        // the id of the fetched artist must match with the 'artistId'
+        assert(fetchedArtist.id == artistId)
     }
 
     @Test
-    fun getAlbumsAssociatedWithArtistTest_validArtistId_returnsAlbumsDTO() = runBlocking {
-        // given an invalid artistId
+    fun getAlbumsAssociatedWithArtistTest_validArtistId_returnsAlbumsDTO() {
+        // given an valid artistId
         val artistId = validArtistId
-        // when fetching the artist
-        val response = musicService.getAlbumsOfArtistWithId(artistId, "IN")
-        // the body of the response should not be null
-        assert(response.body() != null)
+        // the albums associated with the artist must be fetched successfully
+        runBlocking { musicService.getAlbumsOfArtistWithId(artistId, "IN") }
     }
 
     @Test
-    fun getAlbumsAssociatedWithArtistTest_validArtistIdWithLimit_returnsListOfSpecifiedSize() =
-        runBlocking {
-            // given an invalid artistId and limit
-            val artistId = validArtistId
-            // when fetching the artist with limit set to 10
-            val limit = 10
-            val response = musicService.getAlbumsOfArtistWithId(
+    fun getAlbumsAssociatedWithArtistTest_validArtistIdWithLimit_returnsListOfSpecifiedSize() {
+        // given an invalid artistId and limit
+        val artistId = validArtistId
+        // when fetching the artist with limit set to 10
+        val limit = 10
+        val albums = runBlocking {
+            // the albums associated with the artist must be fetched successfully
+            musicService.getAlbumsOfArtistWithId(
                 artistId = artistId,
                 market = "IN",
                 limit = limit
             )
-            // the body of the response should not be null
-            assert(response.body() != null)
-            // the number of items should be equal to the specified limit
-            assert(response.body()!!.items.size == 10)
         }
-
-    @Test
-    fun getTopTracksTest_validArtistId_returnsTopTracksDTO() = runBlocking {
-        // given an valid artistId
-        val artistId = validArtistId
-        // when fetching the top ten tracks
-        val response = musicService.getTopTenTracksForArtistWithId(
-            artistId = artistId,
-            market = "IN"
-        )
-        // the body of the response should not be null
-        assert(response.body() != null)
+        // the number of items should be equal to the specified limit
+        assert(albums.items.size == 10)
     }
 
     @Test
-    fun getAlbumTest_validAlbumId_returnsAlbumDTO() = runBlocking {
+    fun getTopTracksTest_validArtistId_returnsTopTracksDTO() {
+        // given a valid artistId
+        val artistId = validArtistId
+        runBlocking {
+            // the top ten tracks associated with the artist must be
+            // successfully fetched
+            musicService.getTopTenTracksForArtistWithId(artistId = artistId, market = "IN")
+        }
+    }
+
+    @Test
+    fun getAlbumTest_validAlbumId_returnsAlbumDTO() {
         // given an valid albumId
         val albumId = "4aawyAB9vmqN3uQ7FjRGTy"
-        // when fetching the album
-        val response = musicService.getAlbumWithId(
-            albumId = albumId,
-            market = "IN"
-        )
-        // the body of the response should not be null
-        assert(response.body() != null)
+        runBlocking {
+            // the album must be fetched successfully
+            musicService.getAlbumWithId(albumId = albumId, market = "IN")
+        }
     }
 
     @Test
-    fun getAlbumTest_validAlbumId_returnsNull() = runBlocking {
-        // given an invalid albumId
-        val albumId = "-"
-        // when fetching the album
-        val response = musicService.getAlbumWithId(
-            albumId = albumId,
-            market = "IN"
-        )
-        // the body of the response should  be null
-        assert(response.body() == null)
-    }
-
-    @Test
-    fun getPlaylistTest_validPlaylistId_returnsPlaylistDTO() = runBlocking {
-        // given an valid albumId
+    fun getPlaylistTest_validPlaylistId_returnsPlaylistDTO() {
+        // given an valid playlistId
         val albumId = "7sZbq8QGyMnhKPcLJvCUFD"
-        // when fetching the album
-        val response = musicService.getPlaylistWithId(
-            playlistId = albumId,
-            market = "IN"
-        )
-        // the body of the response should not be null
-        assert(response.body() != null)
+        runBlocking {
+            // the playlist must be fetched successfully
+            musicService.getPlaylistWithId(playlistId = albumId, market = "IN")
+        }
     }
 
     @Test
@@ -140,21 +115,21 @@ class SpotifyServiceTest {
     }
 
     @Test
-    fun searchTrackTest_validSearchQueryWithLimit_returnsSearchResultsDTO() = runBlocking {
-
+    fun searchTrackTest_validSearchQueryWithLimit_returnsSearchResultsDTO() {
         // given an valid search query for a track
         val searchQuery = "Visiting hours by Ed Sheeran"
         // when performing search operation
-        val responseBody = musicService.search(
-            searchQuery = searchQuery,
-            type = buildSearchQueryWithTypes(SearchQueryType.TRACK),
-            market = "IN",
-            limit = 1 // limiting the number of results to one
-        ).body()
-        // the body of the response should not be null
-        assert(responseBody != null)
+        val searchResultsDTO = runBlocking {
+            // the search results must be fetched successfully
+            musicService.search(
+                searchQuery = searchQuery,
+                type = buildSearchQueryWithTypes(SearchQueryType.TRACK),
+                market = "IN",
+                limit = 1 // limiting the number of results to one
+            )
+        }
         // all other properties except the 'tracks' property should be null
-        with(responseBody!!) {
+        with(searchResultsDTO) {
             assert(tracks != null)
             assert(albums == null)
             assert(artists == null)
@@ -163,20 +138,21 @@ class SpotifyServiceTest {
     }
 
     @Test
-    fun searchAlbumTest_validSearchQueryWithLimit_returnsSearchResultsDTO() = runBlocking {
+    fun searchAlbumTest_validSearchQueryWithLimit_returnsSearchResultsDTO() {
         // given an valid search query for an album
         val searchQuery = "="
         // when performing search operation
-        val responseBody = musicService.search(
-            searchQuery = searchQuery,
-            type = buildSearchQueryWithTypes(SearchQueryType.ALBUM),
-            market = "IN",
-            limit = 1 // limiting the number of results to one
-        ).body()
-        // the body of the response should not be null
-        assert(responseBody != null)
+        val searchResultsDTO = runBlocking {
+            // the search results must be fetched successfully
+            musicService.search(
+                searchQuery = searchQuery,
+                type = buildSearchQueryWithTypes(SearchQueryType.ALBUM),
+                market = "IN",
+                limit = 1 // limiting the number of results to one
+            )
+        }
         // all other properties except the 'albums' property should be null
-        with(responseBody!!) {
+        with(searchResultsDTO) {
             assert(tracks == null)
             assert(albums != null)
             assert(artists == null)
@@ -185,20 +161,21 @@ class SpotifyServiceTest {
     }
 
     @Test
-    fun searchArtistTest_validSearchQueryWithLimit_returnsSearchResultsDTO() = runBlocking {
+    fun searchArtistTest_validSearchQueryWithLimit_returnsSearchResultsDTO() {
         // given an valid search query for an artist
         val searchQuery = "ed sheeran"
         // when performing search operation
-        val responseBody = musicService.search(
-            searchQuery = searchQuery,
-            type = buildSearchQueryWithTypes(SearchQueryType.ARTIST),
-            market = "IN",
-            limit = 1 // limiting the number of results to one
-        ).body()
-        // the body of the response should not be null
-        assert(responseBody != null)
+        val searchResultsDTO = runBlocking {
+            // the search results must be fetched successfully
+            musicService.search(
+                searchQuery = searchQuery,
+                type = buildSearchQueryWithTypes(SearchQueryType.ARTIST),
+                market = "IN",
+                limit = 1 // limiting the number of results to one
+            )
+        }
         // all other properties except the 'artists' property should be null
-        with(responseBody!!) {
+        with(searchResultsDTO) {
             assert(tracks == null)
             assert(albums == null)
             assert(artists != null)
@@ -207,20 +184,21 @@ class SpotifyServiceTest {
     }
 
     @Test
-    fun searchPlaylistTest_validSearchQueryWithLimit_returnsSearchResultsDTO() = runBlocking {
+    fun searchPlaylistTest_validSearchQueryWithLimit_returnsSearchResultsDTO() {
         // given an valid search query for a playlist
         val searchQuery = "Anirudh"
         // when performing search operation
-        val responseBody = musicService.search(
-            searchQuery = searchQuery,
-            type = buildSearchQueryWithTypes(SearchQueryType.PLAYLIST),
-            market = "IN",
-            limit = 1 // limiting the number of results to one
-        ).body()
-        // the body of the response should not be null
-        assert(responseBody != null)
+        val searchResultsDTO = runBlocking {
+            // the search results must be fetched successfully
+            musicService.search(
+                searchQuery = searchQuery,
+                type = buildSearchQueryWithTypes(SearchQueryType.PLAYLIST),
+                market = "IN",
+                limit = 1 // limiting the number of results to one
+            )
+        }
         // all other properties except the 'playlists' property should be null
-        with(responseBody!!) {
+        with(searchResultsDTO) {
             assert(tracks == null)
             assert(albums == null)
             assert(artists == null)
@@ -229,20 +207,21 @@ class SpotifyServiceTest {
     }
 
     @Test
-    fun searchTestForAllTypes_validSearchQueryWithLimit_returnsSearchResultDTO() = runBlocking {
+    fun searchTestForAllTypes_validSearchQueryWithLimit_returnsSearchResultDTO() {
         // given an valid search query
         val searchQuery = "Anirudh"
         // when performing search operation for all types
-        val responseBody = musicService.search(
-            searchQuery = searchQuery,
-            market = "IN",
-            type = SpotifyEndPoints.Defaults.defaultSearchQueryTypes,
-            limit = 1 // limiting the number of results for each type to one
-        ).body()
-        // the body of the response should not be null
-        assert(responseBody != null)
-        // all the propereties should not be null
-        with(responseBody!!) {
+        val searchResultsDTO = runBlocking {
+            // the search results must be fetched successfully
+            musicService.search(
+                searchQuery = searchQuery,
+                market = "IN",
+                type = SpotifyEndPoints.Defaults.defaultSearchQueryTypes,
+                limit = 1 // limiting the number of results for each type to one
+            )
+        }
+        // all the properties should not be null
+        with(searchResultsDTO) {
             assert(tracks != null)
             assert(albums != null)
             assert(artists != null)
@@ -261,18 +240,16 @@ class SpotifyServiceTest {
      * nullable type.
      */
     @Test(expected = MissingKotlinParameterException::class)
-    fun jacksonDeSerializationTest_nullValueForNonNullableKotlinProperty_throwsException() =
-        runBlocking {
-            // given a json string object with a value of non nullable
-            // kotlin property set to null
-            val jsonString = "{\"id\":null,\"name\":\"s\"}"
-            // when mapping the object to a kotlin class with the same field
-            // declared as non-nullable
-            jacksonObjectMapper().readValue(
-                jsonString,
-                AlbumMetadataDTO.ArtistInfoDTO::class.java
-            )
-            // an exception should be thrown
-            return@runBlocking Unit
-        }
+    fun jacksonDeSerializationTest_nullValueForNonNullableKotlinProperty_throwsException() {
+        // given a json string object with a value of non nullable
+        // kotlin property set to null
+        val jsonString = "{\"id\":null,\"name\":\"s\"}"
+        // when mapping the object to a kotlin class with the same field
+        // declared as non-nullable
+        jacksonObjectMapper().readValue(
+            jsonString,
+            AlbumMetadataDTO.ArtistInfoDTO::class.java
+        )
+        // an exception should be thrown
+    }
 }
