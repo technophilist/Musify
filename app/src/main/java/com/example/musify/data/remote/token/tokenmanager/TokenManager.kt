0fa0c77512a11093bd54954d1b148fb9dc1acc1a
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-07-07 16:46:45 +0530

    refactor: remove SPOTIFY_CLIENT_SECRET_BASE64 const

--- app/src/main/java/com/example/musify/data/remote/token/tokenmanager/TokenManager.kt
+++ app/src/main/java/com/example/musify/data/remote/token/tokenmanager/TokenManager.kt
@@ -1,33 +1,11 @@
 package com.example.musify.data.remote.token.tokenmanager
 
-import android.util.Base64
-import com.example.musify.BuildConfig
 import com.example.musify.data.remote.musicservice.SpotifyEndPoints
 import com.example.musify.data.remote.token.AccessTokenResponseDTO
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.Header
 import retrofit2.http.POST
-
-/**
- * A top level value that contains the client secret associated with
- * spotify's api in a Base64 encoded format. This string has to be
- * included in the header of an authorization request to get an
- * **access token** from the spotify api. This is **not** a token
- * than can be used to directly access spotify's api. It is secret
- * that can be sent to spotify's server's using the [TokenManager.getNewAccessToken]
- * to get the actual token that can be used to communicate with
- * spotify's servers.
- */
-@Deprecated(
-    message = "use getSpotifyClientSecret(base64Encoder: Base64Encoder)",
-    replaceWith = ReplaceWith("getSpotifyClientSecret()")
-)
-val SPOTIFY_CLIENT_SECRET_BASE64: String
-    get() = Base64.encodeToString(
-        "Basic ${BuildConfig.SPOTIFY_CLIENT_ID}:${BuildConfig.SPOTIFY_CLIENT_SECRET}".toByteArray(),
-        Base64.NO_WRAP
-    )
 
 const val defaultGrantType = "client_credentials"
 
