1d1a3afee88a7d6919b13e65923ddea14524de22
Marsh Royden <marshroyden@Marshs-MacBook-Air.local>
2022-07-07 16:38:00 +0530

    fix: append unencoded 'Basic' to encoded string in getSpotifyClientSecret()

--- app/src/main/java/com/example/musify/data/remote/token/SpotifyClientSecret.kt
+++ app/src/main/java/com/example/musify/data/remote/token/SpotifyClientSecret.kt
@@ -10,5 +10,6 @@ import com.example.musify.data.encoder.Base64Encoder
 fun getSpotifyClientSecret(base64Encoder: Base64Encoder): String {
     val clientId = BuildConfig.SPOTIFY_CLIENT_ID
     val clientSecret = BuildConfig.SPOTIFY_CLIENT_SECRET
-    return base64Encoder.encodeToString("Basic $clientId:$clientSecret".toByteArray())
+    val encodedString = base64Encoder.encodeToString("$clientId:$clientSecret".toByteArray())
+    return "Basic $encodedString"
 }
