6473b032a4ad88bfe10973c8629854e7cc1079c0
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-07-08 15:45:26 +0530

    refactor: define MapperImageSize enum

---
+++ app/src/main/java/com/example/musify/data/utils/MapperImageSize.kt
@@ -0,0 +1,7 @@
+package com.example.musify.data.utils
+
+/**
+ * An enum that is used to describe the image size to be used when
+ * mapping an object.
+ */
+enum class MapperImageSize { SMALL, MEDIUM, LARGE }
