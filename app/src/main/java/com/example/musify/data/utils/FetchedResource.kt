c71ab7e458be956139eedb56ce2fd25c66450dc9
Marsh Royden <marshroyden@Marshs-MacBook-Air.local>
2022-07-08 15:30:05 +0530

    refactor: define FetchedResource sealed class hierarchy

---
+++ app/src/main/java/com/example/musify/data/utils/FetchedResource.kt
@@ -0,0 +1,28 @@
+package com.example.musify.data.utils
+
+/**
+ * A sealed class hierarchy that is used to encapsulate a fetched
+ * resource of type [ResourceType]. If any error occurs, the
+ * [FailureType] will be used.
+ */
+sealed class FetchedResource<ResourceType, FailureType> {
+    /**
+     * A class that encapsulates a successful fetch of a resource of
+     * type [ResourceType]. The [data] property  holds the resource.
+     */
+    data class Success<ResourceType, FailureType>(
+        private val data: ResourceType
+    ) : FetchedResource<ResourceType, FailureType>()
+
+    /**
+     * A class that encapsulates an un-successful fetch operation
+     * of a resource of type [ResourceType]. The [error] property
+     * contains the [FailureType]. The [data] property contains an
+     * optional resource that can be used to speify the data to be
+     * used in the case of failure.
+     */
+    data class Failure<ResourceType, FailureType>(
+        private val error: FailureType,
+        private val data: ResourceType? = null
+    ) : FetchedResource<ResourceType, FailureType>()
+}
