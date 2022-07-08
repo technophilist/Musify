b64c01efc704c99856f9be44ece01098a47af53d
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-07-08 15:48:44 +0530

    refactor(ArtistDTO): add toArtistSummary() extension

--- app/src/main/java/com/example/musify/data/dto/ArtistDTO.kt
+++ app/src/main/java/com/example/musify/data/dto/ArtistDTO.kt
@@ -1,5 +1,9 @@
 package com.example.musify.data.dto
 
+import com.example.musify.data.utils.MapperImageSize
+import com.example.musify.domain.MusicSummary
+import java.net.URL
+
 
 /**
  * A DTO object that contains information about an Artist.
@@ -17,3 +21,20 @@ data class ArtistDTO(
     data class Followers(val total: String)
 }
 
+/**
+ * A mapper function used to map an instance of [ArtistDTO] to
+ * an instance of [MusicSummary.ArtistSummary]. The [imageSize]
+ * parameter describes the size of image to be used for the
+ * [MusicSummary.ArtistSummary] instance.
+ */
+fun ArtistDTO.toArtistSummary(imageSize: MapperImageSize) = MusicSummary.ArtistSummary(
+    id = id,
+    name = name,
+    associatedImageUrl = URL(
+        when (imageSize) {
+            MapperImageSize.SMALL -> images[2]
+            MapperImageSize.MEDIUM -> images[1]
+            MapperImageSize.LARGE -> images[0]
+        }.url
+    )
+)
