bf5ffc8a6d04b2bb2174067c86c8657a7fa5ef2b
Marsh Royden <marshroyden@Marshs-MacBook-Air.local>
2022-07-09 23:04:19 +0530

    refactor: define SpotifyBaseUrls object

---
+++ app/src/main/java/com/example/musify/data/remote/musicservice/SpotifyBaseUrls.kt
@@ -0,0 +1,10 @@
+package com.example.musify.data.remote.musicservice
+
+/**
+ * An object that contains the different base URL's used to create
+ * an instance of [SpotifyService].
+ */
+object SpotifyBaseUrls {
+    const val API_URL = "https://api.spotify.com/"
+    const val AUTHENTICATION_URL = "https://accounts.spotify.com/"
+}
