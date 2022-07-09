e1352576379d04e2d673cee8090ed937cd7b2d7f
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-07-09 16:59:01 +0530

    refactor(PlatlistMetadataDTO): add toPlaylistSummary() extension

--- app/src/main/java/com/example/musify/data/dto/PlaylistMetadataDTO.kt
+++ app/src/main/java/com/example/musify/data/dto/PlaylistMetadataDTO.kt
@@ -1,6 +1,10 @@
 package com.example.musify.data.dto
 
+import com.example.musify.data.utils.MapperImageSize
+import com.example.musify.data.utils.getImageDtoForImageSize
+import com.example.musify.domain.MusicSummary
 import com.fasterxml.jackson.annotation.JsonProperty
+import java.net.URL
 
 /**
  * A DTO object that contains only the metadata associated with a
@@ -12,4 +16,18 @@ data class PlaylistMetadataDTO(
     val name: String,
     val images: List<ImageDTO>,
     @JsonProperty("owner") val ownerName: PlaylistDTO.OwnerNameWrapper
+)
+
+/**
+ * A mapper function used to map an instance of [PlaylistMetadataDTO] to
+ * an instance of [MusicSummary.PlaylistSummary]. The [imageSize]
+ * parameter describes the size of image to be used for the
+ * [MusicSummary.PlaylistSummary] instance.
+ */
+fun PlaylistMetadataDTO.toPlaylistSummary(
+    imageSize: MapperImageSize
+) = MusicSummary.PlaylistSummary(
+    id = id,
+    name = name,
+    associatedImageUrl = URL(images.getImageDtoForImageSize(imageSize).url)
 )
