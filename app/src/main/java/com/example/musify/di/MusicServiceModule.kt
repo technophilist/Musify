9fc0819cabf51c24f1ecb49ba4288d856bc7321f
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-07-09 23:05:14 +0530

    refactor(MusicServiceModule): use constsdefined in SpotifyBaseUrls

--- app/src/main/java/com/example/musify/di/MusicServiceModule.kt
+++ app/src/main/java/com/example/musify/di/MusicServiceModule.kt
@@ -1,5 +1,6 @@
 package com.example.musify.di
 
+import com.example.musify.data.remote.musicservice.SpotifyBaseUrls
 import com.example.musify.data.remote.musicservice.SpotifyService
 import com.example.musify.data.remote.token.tokenmanager.TokenManager
 import com.example.musify.utils.defaultMusifyJacksonConverterFactory
@@ -16,7 +17,7 @@ object MusicServiceModule {
     @Provides
     @Singleton
     fun provideSpotifyService(): SpotifyService = Retrofit.Builder()
-        .baseUrl("https://api.spotify.com/") // TODO move const to separate file
+        .baseUrl(SpotifyBaseUrls.API_URL)
         .addConverterFactory(defaultMusifyJacksonConverterFactory)
         .build()
         .create(SpotifyService::class.java)
@@ -24,7 +25,7 @@ object MusicServiceModule {
     @Provides
     @Singleton
     fun provideTokenManager(): TokenManager = Retrofit.Builder()
-        .baseUrl("https://accounts.spotify.com/")// TODO move const to separate file
+        .baseUrl(SpotifyBaseUrls.AUTHENTICATION_URL)
         .addConverterFactory(defaultMusifyJacksonConverterFactory)
         .build()
         .create(TokenManager::class.java)
