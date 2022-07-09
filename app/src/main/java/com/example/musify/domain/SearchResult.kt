86492fc8ae2894d9983a2a80cf9f06353eb6de88
Marsh Royden <marshroyden@Marshs-MacBook-Air.local>
2022-07-09 16:49:06 +0530

    refactor: define SearchResult domain class

---
+++ app/src/main/java/com/example/musify/domain/SearchResult.kt
@@ -0,0 +1,12 @@
+package com.example.musify.domain
+
+/**
+ * A class that models a search result. It contains all the [tracks],
+ * [albums],[artists] and [playlists] that matched a search query.
+ */
+data class SearchResult(
+    val tracks:List<MusicSummary.TrackSummary>,
+    val albums:List<MusicSummary.AlbumSummary>,
+    val artists:List<MusicSummary.ArtistSummary>,
+    val playlists:List<MusicSummary.PlaylistSummary>,
+)
