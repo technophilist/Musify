265ed0783ce55e38ff483ebac34f3fd0b60ca9bf
Marsh Royden <marshroyden@Marshs-MacBook-Air.local>
2022-07-09 17:13:13 +0530

    refactor: define Repository interface

---
+++ app/src/main/java/com/example/musify/data/repository/Repository.kt
@@ -0,0 +1,47 @@
+package com.example.musify.data.repository
+
+import com.example.musify.data.utils.FetchedResource
+import com.example.musify.data.utils.MapperImageSize
+import com.example.musify.domain.MusicSummary
+import com.example.musify.domain.MusifyHttpErrorType
+import com.example.musify.domain.SearchResult
+
+/**
+ * An interface the consists of all the methods that are a requisite for
+ * an instance of [Repository].
+ */
+interface Repository {
+    suspend fun fetchArtistSummaryForId(
+        artistId: String,
+        imageSize: MapperImageSize
+    ): FetchedResource<MusicSummary.ArtistSummary, MusifyHttpErrorType>
+
+    suspend fun fetchAlbumsOfArtistWithId(
+        artistId: String,
+        imageSize: MapperImageSize,
+        countryCode: String
+    ): FetchedResource<List<MusicSummary.AlbumSummary>, MusifyHttpErrorType>
+
+    suspend fun fetchTopTenTracksForArtistWithId(
+        artistId: String,
+        imageSize: MapperImageSize,
+        countryCode: String
+    ): FetchedResource<List<MusicSummary.TrackSummary>, MusifyHttpErrorType>
+
+    suspend fun fetchAlbumWithId(
+        albumId: String,
+        imageSize: MapperImageSize,
+        countryCode: String
+    ): FetchedResource<MusicSummary.AlbumSummary, MusifyHttpErrorType>
+
+    suspend fun fetchPlaylistWithId(
+        playlistId: String,
+        countryCode: String
+    ): FetchedResource<MusicSummary.PlaylistSummary, MusifyHttpErrorType>
+
+    suspend fun fetchSearchResultsForQuery(
+        searchQuery: String,
+        imageSize: MapperImageSize,
+        countryCode: String
+    ): FetchedResource<SearchResult, MusifyHttpErrorType>
+}
