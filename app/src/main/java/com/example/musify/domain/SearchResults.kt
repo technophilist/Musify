ab66cdf944768cfe7a8b6daac8e32512d0532abc
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-07-10 12:56:19 +0530

    refactor(SearchResults): replace all properties with *SearchResult classes

--- app/src/main/java/com/example/musify/domain/SearchResults.kt
+++ app/src/main/java/com/example/musify/domain/SearchResults.kt
@@ -1,12 +1,17 @@
 package com.example.musify.domain
 
+import com.example.musify.domain.searchresult.AlbumSearchResult
+import com.example.musify.domain.searchresult.ArtistSearchResult
+import com.example.musify.domain.searchresult.PlaylistSearchResult
+import com.example.musify.domain.searchresult.TrackSearchResult
+
 /**
  * A class that models a search result. It contains all the [tracks],
  * [albums],[artists] and [playlists] that matched a search query.
  */
 data class SearchResults(
-    val tracks:List<MusicSummary.TrackSummary>,
-    val albums:List<MusicSummary.AlbumSummary>,
-    val artists:List<MusicSummary.ArtistSummary>,
-    val playlists:List<MusicSummary.PlaylistSummary>,
+    val tracks: List<TrackSearchResult>,
+    val albums: List<AlbumSearchResult>,
+    val artists: List<ArtistSearchResult>,
+    val playlists: List<PlaylistSearchResult>,
 )
