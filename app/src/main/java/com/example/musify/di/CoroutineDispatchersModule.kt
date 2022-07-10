b66354e88ce5037f7171c91c6076428ad308f909
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-07-10 15:58:06 +0530

    refactor: add CoroutineDispatchersModule

---
+++ app/src/main/java/com/example/musify/di/CoroutineDispatchersModule.kt
@@ -0,0 +1,33 @@
+package com.example.musify.di
+
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityRetainedComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Qualifier
+
+@Qualifier
+annotation class MainDispatcher
+
+@Qualifier
+annotation class IODispatcher
+
+@Qualifier
+annotation class DefaultDispatcher
+
+
+@InstallIn(ActivityRetainedComponent::class)
+object CoroutineDispatchersModule {
+    @Provides
+    @DefaultDispatcher
+    fun provideDefaultDispatcher(): CoroutineDispatcher = Dispatchers.Default
+
+    @Provides
+    @IODispatcher
+    fun provideIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
+
+    @Provides
+    @MainDispatcher
+    fun provideMainDispatcher(): CoroutineDispatcher = Dispatchers.Main
+}
