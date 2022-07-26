dfe8e5371c2e1ea918de32c0d40df7b4febef0b2
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-07-26 10:07:21 +0530

    refactor!: remove 'coverArtUrl' property of Genre class

--- app/src/main/java/com/example/musify/domain/Genre.kt
+++ app/src/main/java/com/example/musify/domain/Genre.kt
@@ -1,16 +1,11 @@
 package com.example.musify.domain
 
-import java.net.URL
-
 /**
  * A class that models a specific genre.
  * @param id unique id of the genre
  * @param name the name of the genre
- * @param coverArtUrl an instance of [URL] that specifies the url for
- * the cover art associated with the genre.
  */
 data class Genre(
     val id: String,
-    val name: String,
-    val coverArtUrl: URL
+    val name: String
 )
