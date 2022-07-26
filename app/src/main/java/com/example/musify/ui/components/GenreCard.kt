bd7c3a87a7096f9d1ba963441af8598acaa2b6aa
Marsh Royden <marshroyden@Marshs-MacBook-Air.local>
2022-07-26 11:37:09 +0530

    refactor: deprecate old GenreCard composable & add new overload

--- app/src/main/java/com/example/musify/ui/components/GenreCard.kt
+++ app/src/main/java/com/example/musify/ui/components/GenreCard.kt
@@ -1,8 +1,8 @@
 package com.example.musify.ui.components
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Card
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
@@ -10,7 +10,10 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.example.musify.domain.Genre
@@ -33,6 +36,7 @@ import com.example.musify.domain.Genre
  */
 @ExperimentalMaterialApi
 @Composable
+@Deprecated("Use other overload")
 fun GenreCard(
     genre: Genre,
     modifier: Modifier = Modifier,
@@ -55,6 +59,53 @@ fun GenreCard(
                 isLoadingPlaceholderVisible = isLoadingPlaceholderVisible,
                 onImageLoading = onImageLoading,
                 onImageLoadingFinished = onImageLoadingFinished
+            )
+            Text(
+                modifier = Modifier
+                    .align(Alignment.TopStart)
+                    .padding(16.dp),
+                text = genre.name,
+                fontWeight = FontWeight.Bold,
+                style = MaterialTheme.typography.h6
+            )
+        }
+    }
+}
+
+/**
+ * A card that represents a Genre.
+ *
+ * @param genre the instance of [Genre] that the card is modeling.
+ * @param imageResourceId a resource id that specifies the image to
+ * be loaded as the thumbnail of the genre card.
+ * @param modifier modifier to be applied to this composable.
+ * @param onClick the lambda to execute when the card is clicked.
+ * @param backgroundColor the background color.
+ */
+@ExperimentalMaterialApi
+@Composable
+fun GenreCard(
+    genre: Genre,
+    @DrawableRes imageResourceId: Int,
+    modifier: Modifier = Modifier,
+    onClick: (() -> Unit)? = null,
+    backgroundColor: Color = MaterialTheme.colors.surface
+) {
+    Card(
+        modifier = modifier,
+        onClick = onClick ?: {},
+        backgroundColor = backgroundColor
+    ) {
+        Box(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Image(
+                modifier = Modifier
+                    .size(90.dp)
+                    .offset(x = 110.dp, y = 30.dp)
+                    .rotate(30f),
+                painter = painterResource(id = imageResourceId),
+                contentDescription = null
             )
             Text(
                 modifier = Modifier
