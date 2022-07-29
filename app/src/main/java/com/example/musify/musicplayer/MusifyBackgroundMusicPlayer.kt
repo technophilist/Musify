4180baa57ce5423cb6f2b7b20bf19b6b6a2c3e13
Marsh Royden <marshroyden@Marshs-MacBook-Air.local>
2022-07-29 16:26:28 +0530

    refactor(MusicPlayer)!: add nested 'Track' class & add playTrack method

--- app/src/main/java/com/example/musify/musicplayer/MusifyBackgroundMusicPlayer.kt
+++ app/src/main/java/com/example/musify/musicplayer/MusifyBackgroundMusicPlayer.kt
@@ -1,6 +1,7 @@
 package com.example.musify.musicplayer
 
 import android.content.Context
+import com.example.musify.musicplayer.utils.MediaDescriptionAdapter
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.ui.PlayerNotificationManager
@@ -13,15 +14,31 @@ class MusifyBackgroundMusicPlayer @Inject constructor(
     @ApplicationContext context: Context,
     private val exoPlayer: ExoPlayer
 ) : MusicPlayer {
+    private var currentlyPlayingTrack: MusicPlayer.Track? = null
     private val notificationManager =
         PlayerNotificationManager.Builder(context, NOTIFICATION_ID, NOTIFICATION_CHANNEL_ID)
             .setChannelImportance(NotificationUtil.IMPORTANCE_LOW)
+            .setMediaDescriptionAdapter(MediaDescriptionAdapter(
+                getCurrentContentText = { currentlyPlayingTrack?.artistsString ?: "" },
+                getCurrentContentTitle = { currentlyPlayingTrack?.title ?: "" },
+                getCurrentLargeIcon = { _, _ -> null }
+            ))
             .build().apply { setPlayer(exoPlayer) }
 
     override fun playTrackFromUrlString(urlString: String) {
         with(exoPlayer) {
             if (isPlaying) exoPlayer.stop()
             setMediaItem(MediaItem.fromUri(urlString))
+            prepare()
+            play()
+        }
+    }
+
+    override fun playTrack(track: MusicPlayer.Track) {
+        with(exoPlayer) {
+            if (isPlaying) exoPlayer.stop()
+            currentlyPlayingTrack = track
+            setMediaItem(MediaItem.fromUri(track.trackUrlString))
             prepare()
             play()
         }
