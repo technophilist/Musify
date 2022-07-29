12118be5cd5b97934f07eb197e4e23b767b699a7
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-07-29 16:26:28 +0530

    refactor(MusicPlayer)!: add nested 'Track' class & add playTrack method

--- app/src/main/java/com/example/musify/musicplayer/MusicPlayer.kt
+++ app/src/main/java/com/example/musify/musicplayer/MusicPlayer.kt
@@ -1,7 +1,17 @@
 package com.example.musify.musicplayer
 
+import android.graphics.Bitmap
+
 interface MusicPlayer {
     fun playTrackFromUrlString(urlString: String)
     fun pauseCurrentlyPlayingTrack()
     fun stopPlayingTrack()
+    fun playTrack(track: Track)
+    data class Track(
+        val id: String,
+        val title: String,
+        val artistsString: String,
+        val albumArt: Bitmap,
+        val trackUrlString:String
+    )
 }
