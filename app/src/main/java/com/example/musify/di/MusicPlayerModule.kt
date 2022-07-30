bf836530e6808e07ef52784fe774a7a710051db3
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-07-30 13:02:11 +0530

    docs(MusicPlayerModule): add kdoc explaining why dependencies are unscoped

--- app/src/main/java/com/example/musify/di/MusicPlayerModule.kt
+++ app/src/main/java/com/example/musify/di/MusicPlayerModule.kt
@@ -9,6 +9,13 @@ import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
 
+/**
+ * Note: The dependencies are not scoped because the underlying
+ * media player is always a singleton. [ExoPlayerModule.provideExoplayer]
+ * is annotated with @Singleton, therefore any class that depends on it
+ * need to be a singleton since the class will be provided the same
+ * instance.
+ */
 @Module
 @InstallIn(ViewModelComponent::class)
 abstract class MusicPlayerModule {
