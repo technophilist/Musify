85117391b8e5549e84e6a025f5c0dcaf45d8461e
Marsh Royden <marshroyden@Marshs-MacBook-Air.local>
2022-08-12 16:31:33 +0530

    test: add SpotifySearchPagingSourceTest

---
+++ app/src/test/java/com/example/musify/data/paging/FakeSearchResultsPages.kt
@@ -0,0 +1,21 @@
+package com.example.musify.data.paging
+
+import com.example.musify.data.remote.response.*
+
+enum class FakeSearchResultsPageNumbers(val pageIndexValue: Int) {
+    PAGE_1(0),
+    PAGE_2(1),
+    PAGE_3(2)
+}
+
+private val fakeTracks = List(3) { fakeTrackResponseWithAlbumMetadata.copy(id = it.toString()) }
+private val fakeAlbums = List(3) { fakeAlbumMetadataResponse.copy(id = it.toString()) }
+private val fakeArtists = List(3) { fakeArtistResponse.copy(id = it.toString()) }
+private val fakePlaylists = List(3) { fakePlaylistMetadataResponse.copy(id = it.toString()) }
+
+fun getSearchResultsResponseForFakePageNumber(fakePageNumber: FakeSearchResultsPageNumbers) = SearchResultsResponse(
+    tracks = SearchResultsResponse.Tracks(listOf(fakeTracks[fakePageNumber.pageIndexValue])),
+    albums = SearchResultsResponse.Albums(listOf(fakeAlbums[fakePageNumber.pageIndexValue])),
+    artists = SearchResultsResponse.Artists(listOf(fakeArtists[fakePageNumber.pageIndexValue])),
+    playlists = SearchResultsResponse.Playlists(listOf(fakePlaylists[fakePageNumber.pageIndexValue]))
+)
