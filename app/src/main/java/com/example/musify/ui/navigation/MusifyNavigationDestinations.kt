813631fc7ad6dc6d7fe5a24a2378bdee8098cd83
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-08-16 15:03:12 +0530

    fix: fix erroneous non-matching arg name in MusifyDestinations.ArtistDetailScreen

--- app/src/main/java/com/example/musify/ui/navigation/MusifyNavigationDestinations.kt
+++ app/src/main/java/com/example/musify/ui/navigation/MusifyNavigationDestinations.kt
@@ -5,7 +5,7 @@ sealed class MusifyNavigationDestinations(val route: String) {
         MusifyNavigationDestinations("MusifyNavigationDestinations.SearchScreen")
 
     object ArtistDetailScreen :
-        MusifyNavigationDestinations("MusifyNavigationDestinations.ArtistDetailScreen/{artistId}/{name}/encodedImageUrlString") {
+        MusifyNavigationDestinations("MusifyNavigationDestinations.ArtistDetailScreen/{artistId}/{artistName}/encodedImageUrlString") {
         const val NAV_ARG_ARTIST_ID = "artistId"
         const val NAV_ARG_ARTIST_NAME = "artistName"
         const val NAV_ARG_ENCODED_IMAGE_URL_STRING = "encodedImageUrlString"
