095fb87582f0f496be5a1f8f53b4bdc7c5ffd081
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-08-16 16:11:01 +0530

    feat(ArtistDetailScreen): add loading animation

--- app/src/main/java/com/example/musify/ui/screens/ArtistDetailScreen.kt
+++ app/src/main/java/com/example/musify/ui/screens/ArtistDetailScreen.kt
@@ -1,11 +1,15 @@
 package com.example.musify.ui.screens
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -21,6 +25,9 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.itemsIndexed
+import com.airbnb.lottie.LottieComposition
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieConstants
 import com.example.musify.domain.SearchResult
 import com.example.musify.ui.components.AsyncImageWithPlaceholder
 import com.example.musify.ui.components.ListItemCardType
@@ -39,79 +46,97 @@ fun ArtistDetailScreen(
     onPlayButtonClicked: () -> Unit,
     onTrackClicked: (SearchResult.TrackSearchResult) -> Unit,
     onAlbumClicked: (SearchResult.AlbumSearchResult) -> Unit,
+    isLoading: Boolean,
+    loadingAnimationComposition: LottieComposition?,
 ) {
     val subtitleTextColorWithAlpha = MaterialTheme.colors.onBackground.copy(
         alpha = ContentAlpha.disabled
     )
     var isCoverArtPlaceholderVisible by remember { mutableStateOf(false) }
-    LazyColumn(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.spacedBy(16.dp),
-    ) {
-        artistCoverArtHeaderItem(
-            artistName = artistName,
-            artistCoverArtUrlString = artistImageUrlString, // TODO
-            onBackButtonClicked = onBackButtonClicked,
-            onPLayButtonClick = onPlayButtonClicked,
-            isLoadingPlaceholderVisible = isCoverArtPlaceholderVisible,
-            onCoverArtLoading = { isCoverArtPlaceholderVisible = true },
-            onCoverArtLoaded = { isCoverArtPlaceholderVisible = false }
-        )
-        item {
-            SubtitleText(
-                modifier = Modifier.padding(start = 16.dp),
-                text = "Popular tracks"
+    Box {
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
+            artistCoverArtHeaderItem(
+                artistName = artistName,
+                artistCoverArtUrlString = artistImageUrlString, // TODO
+                onBackButtonClicked = onBackButtonClicked,
+                onPLayButtonClick = onPlayButtonClicked,
+                isLoadingPlaceholderVisible = isCoverArtPlaceholderVisible,
+                onCoverArtLoading = { isCoverArtPlaceholderVisible = true },
+                onCoverArtLoaded = { isCoverArtPlaceholderVisible = false }
             )
-        }
-        items(popularTracks) {
-            MusifyCompactListItemCard(
-                modifier = Modifier
-                    .height(64.dp)
-                    .padding(horizontal = 16.dp),
-                cardType = ListItemCardType.TRACK,
-                thumbnailImageUrlString = it.imageUrlString,
-                title = it.name,
-                subtitle = it.artistsString,
-                subtitleTextStyle = MaterialTheme.typography
-                    .caption
-                    .copy(color = subtitleTextColorWithAlpha),
-                onClick = { onTrackClicked(it) },
-                onTrailingButtonIconClick = { /*TODO*/ }
-            )
-        }
-        item {
-            SubtitleText(
-                modifier = Modifier.padding(start = 16.dp),
-                text = "Releases"
-            )
-        }
-        itemsIndexed(
-            items = releases,
-            key = { index, album -> "$index$album" }
-        ) { _, album ->
-            album?.let {
-                MusifyCompactListItemCard(
-                    modifier = Modifier
-                        .height(80.dp)
-                        .padding(horizontal = 16.dp),
-                    cardType = ListItemCardType.ALBUM,
-                    thumbnailImageUrlString = it.albumArtUrlString,
-                    title = it.name,
-                    titleTextStyle = MaterialTheme.typography.h6,
-                    subtitle = it.yearOfReleaseString,
-                    subtitleTextStyle = MaterialTheme.typography
-                        .subtitle2
-                        .copy(color = subtitleTextColorWithAlpha),
-                    onClick = { onAlbumClicked(it) },
-                    onTrailingButtonIconClick = { onAlbumClicked(it) }
+            item {
+                SubtitleText(
+                    modifier = Modifier.padding(start = 16.dp),
+                    text = "Popular tracks"
                 )
             }
+            items(popularTracks) {
+                MusifyCompactListItemCard(
+                    modifier = Modifier
+                        .height(64.dp)
+                        .padding(horizontal = 16.dp),
+                    cardType = ListItemCardType.TRACK,
+                    thumbnailImageUrlString = it.imageUrlString,
+                    title = it.name,
+                    subtitle = it.artistsString,
+                    subtitleTextStyle = MaterialTheme.typography
+                        .caption
+                        .copy(color = subtitleTextColorWithAlpha),
+                    onClick = { onTrackClicked(it) },
+                    onTrailingButtonIconClick = { /*TODO*/ }
+                )
+            }
+            item {
+                SubtitleText(
+                    modifier = Modifier.padding(start = 16.dp),
+                    text = "Releases"
+                )
+            }
+            itemsIndexed(
+                items = releases,
+                key = { index, album -> "$index$album" }
+            ) { _, album ->
+                album?.let {
+                    MusifyCompactListItemCard(
+                        modifier = Modifier
+                            .height(80.dp)
+                            .padding(horizontal = 16.dp),
+                        cardType = ListItemCardType.ALBUM,
+                        thumbnailImageUrlString = it.albumArtUrlString,
+                        title = it.name,
+                        titleTextStyle = MaterialTheme.typography.h6,
+                        subtitle = it.yearOfReleaseString,
+                        subtitleTextStyle = MaterialTheme.typography
+                            .subtitle2
+                            .copy(color = subtitleTextColorWithAlpha),
+                        onClick = { onAlbumClicked(it) },
+                        onTrailingButtonIconClick = { onAlbumClicked(it) }
+                    )
+                }
+            }
+            item {
+                Spacer(modifier = Modifier.navigationBarsHeight())
+            }
         }
-        item {
-            Spacer(modifier = Modifier.navigationBarsHeight())
+        AnimatedVisibility(
+            modifier = Modifier
+                .size(128.dp) // the actual size will be mush lesser because of padding and offset
+                .align(Alignment.Center)
+                .clip(RoundedCornerShape(5))
+                .background(Color.Black.copy(alpha = 0.3f)),
+            visible = isLoading,
+            enter = fadeIn(),
+            exit = fadeOut()
+        ) {
+            LottieAnimation(
+                composition = loadingAnimationComposition,
+                iterations = LottieConstants.IterateForever
+            )
         }
     }
-
 }
 
 @Composable
