b43250e4298a70f7eb9a220b325c28c40e5516aa
Marsh Royden <marshroyden@Marshs-MacBook-Air.local>
2022-08-16 13:37:51 +0530

    refactor: add MusifyNavigationDestinations object

---
+++ app/src/main/java/com/example/musify/ui/navigation/MusifyNavigationDestinations.kt
@@ -0,0 +1,19 @@
+package com.example.musify.ui.navigation
+
+sealed class MusifyNavigationDestinations(val route: String) {
+    object SearchScreen :
+        MusifyNavigationDestinations("MusifyNavigationDestinations.SearchScreen")
+
+    object ArtistDetailScreen :
+        MusifyNavigationDestinations("MusifyNavigationDestinations.ArtistDetailScreen/{artistId}/{name}/encodedImageUrlString") {
+        const val NAV_ARG_ARTIST_ID = "artistId"
+        const val NAV_ARG_ARTIST_NAME = "artistName"
+        const val NAV_ARG_ENCODED_IMAGE_URL_STRING = "encodedImageUrlString"
+        fun buildRoute(
+            artistId: String,
+            artistName: String,
+            encodedImageUrlString: String
+        ) =
+            "MusifyNavigationDestinations.ArtistDetailScreen/$artistId/$artistName/$encodedImageUrlString"
+    }
+}
