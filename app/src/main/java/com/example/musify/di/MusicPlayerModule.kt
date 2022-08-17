a5ac6fd36f78130096e6b0e0fd0d4f8326a90a04
Marsh Royden <marshroyden@Marshs-MacBook-Air.local>
2022-08-17 11:00:54 +0530

    docs(MusicPlayerModule): fix javadoc mistake related to not needing a singleton

--- app/src/main/java/com/example/musify/di/MusicPlayerModule.kt
+++ app/src/main/java/com/example/musify/di/MusicPlayerModule.kt
@@ -13,8 +13,8 @@ import dagger.hilt.android.components.ViewModelComponent
  * Note: The dependencies are not scoped because the underlying
  * media player is always a singleton. [ExoPlayerModule.provideExoplayer]
  * is annotated with @Singleton, therefore any class that depends on it
- * need to be a singleton since the class will be provided the same
- * instance.
+ * need not be a singleton since the class will be provided the same
+ * instance of ExoPlayer.
  */
 @Module
 @InstallIn(ViewModelComponent::class)
