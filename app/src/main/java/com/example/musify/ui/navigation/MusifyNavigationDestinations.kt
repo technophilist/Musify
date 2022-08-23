969b3970f2f17033ef3ad075ff937f220b768075
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-08-23 14:01:48 +0530

    refactor: add AlbumDetailScreen navigation destination

--- app/src/main/java/com/example/musify/ui/navigation/MusifyNavigationDestinations.kt
+++ app/src/main/java/com/example/musify/ui/navigation/MusifyNavigationDestinations.kt
@@ -26,4 +26,20 @@ sealed class MusifyNavigationDestinations(val route: String) {
         }
 
     }
+
+    object AlbumDetailScreen :
+        MusifyNavigationDestinations("MusifyNavigationDestinations.AlbumDetailScreen/{albumId}/{albumName}/{artistsString}/{encodedImageUrlString}") {
+        const val NAV_ARG_ALBUM_ID = "albumId"
+        const val NAV_ARG_ALBUM_NAME = "albumName"
+        const val NAV_ARG_ARTISTS_STRING = "artistsString"
+        const val NAV_ARG_ENCODED_IMAGE_URL_STRING = "encodedImageUrlString"
+
+        fun buildRoute(albumSearchResult: SearchResult.AlbumSearchResult): String {
+            val encodedImageUrlString = URLEncoder.encode(
+                albumSearchResult.albumArtUrlString,
+                StandardCharsets.UTF_8.toString()
+            )
+            return "MusifyNavigationDestinations.AlbumDetailScreen/${albumSearchResult.id}/${albumSearchResult.name}/${albumSearchResult.artistsString}/${encodedImageUrlString}"
+        }
+    }
 }
