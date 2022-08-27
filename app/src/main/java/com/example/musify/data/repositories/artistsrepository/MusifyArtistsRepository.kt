519754ce9430aaabeea60d8e166d4764ebad6bdc
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-08-27 12:14:09 +0530

    refactor: define MusifyArtistsRepository

---
+++ app/src/main/java/com/example/musify/data/repositories/artistsrepository/MusifyArtistsRepository.kt
@@ -0,0 +1,24 @@
+package com.example.musify.data.repositories.artistsrepository
+
+import com.example.musify.data.remote.musicservice.SpotifyService
+import com.example.musify.data.remote.response.toArtistSearchResult
+import com.example.musify.data.repository.tokenrepository.TokenRepository
+import com.example.musify.data.repository.tokenrepository.runCatchingWithToken
+import com.example.musify.data.utils.FetchedResource
+import com.example.musify.data.utils.MapperImageSize
+import com.example.musify.domain.MusifyErrorType
+import com.example.musify.domain.SearchResult
+
+class MusifyArtistsRepository(
+    private val spotifyService: SpotifyService,
+    private val tokenRepository: TokenRepository
+) : ArtistsRepository {
+
+    override suspend fun fetchArtistSummaryForId(
+        artistId: String,
+        imageSize: MapperImageSize
+    ): FetchedResource<SearchResult.ArtistSearchResult, MusifyErrorType> =
+        tokenRepository.runCatchingWithToken {
+            spotifyService.getArtistInfoWithId(artistId, it).toArtistSearchResult(imageSize)
+        }
+}
