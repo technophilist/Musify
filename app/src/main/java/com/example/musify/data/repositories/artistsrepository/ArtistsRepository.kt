238c0256598719ebbb109d793895df815698dbca
Marsh Royden <marshroyden@Marshs-MacBook-Air.local>
2022-08-27 12:13:33 +0530

    refactor(ArtistsRepository)!: replace use of ArtistSummary with ArtistSearchResult

--- app/src/main/java/com/example/musify/data/repositories/artistsrepository/ArtistsRepository.kt
+++ app/src/main/java/com/example/musify/data/repositories/artistsrepository/ArtistsRepository.kt
@@ -2,13 +2,13 @@ package com.example.musify.data.repositories.artistsrepository
 
 import com.example.musify.data.utils.FetchedResource
 import com.example.musify.data.utils.MapperImageSize
-import com.example.musify.domain.MusicSummary
 import com.example.musify.domain.MusifyErrorType
+import com.example.musify.domain.SearchResult
 
 interface ArtistsRepository {
     suspend fun fetchArtistSummaryForId(
         artistId: String,
         imageSize: MapperImageSize
-    ): FetchedResource<MusicSummary.ArtistSummary, MusifyErrorType>
+    ): FetchedResource<SearchResult.ArtistSearchResult, MusifyErrorType>
 
 }
