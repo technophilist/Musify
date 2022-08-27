f4ee9c59e20a534f9314a29c68e694e4888ecd37
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-08-27 12:38:06 +0530

    refactor: define MusifyTracksRepository class

---
+++ app/src/main/java/com/example/musify/data/repositories/tracksrepository/MusifyTracksRepository.kt
@@ -0,0 +1,57 @@
+package com.example.musify.data.repositories.tracksrepository
+
+import com.example.musify.data.remote.musicservice.SpotifyService
+import com.example.musify.data.remote.response.getTracks
+import com.example.musify.data.remote.response.toTrackSearchResult
+import com.example.musify.data.repository.tokenrepository.TokenRepository
+import com.example.musify.data.repository.tokenrepository.runCatchingWithToken
+import com.example.musify.data.utils.FetchedResource
+import com.example.musify.data.utils.MapperImageSize
+import com.example.musify.domain.Genre
+import com.example.musify.domain.MusifyErrorType
+import com.example.musify.domain.SearchResult
+import com.example.musify.domain.toSupportedSpotifyGenreType
+
+class MusifyTracksRepository(
+    private val tokenRepository: TokenRepository,
+    private val spotifyService: SpotifyService
+) : TracksRepository {
+    override suspend fun fetchTopTenTracksForArtistWithId(
+        artistId: String,
+        imageSize: MapperImageSize,
+        countryCode: String
+    ): FetchedResource<List<SearchResult.TrackSearchResult>, MusifyErrorType> =
+        tokenRepository.runCatchingWithToken {
+            spotifyService.getTopTenTracksForArtistWithId(
+                artistId = artistId,
+                market = countryCode,
+                token = it,
+            ).value.map { trackDTOWithAlbumMetadata ->
+                trackDTOWithAlbumMetadata.toTrackSearchResult(imageSize)
+            }
+        }
+
+    override suspend fun fetchTracksForGenre(
+        genre: Genre,
+        imageSize: MapperImageSize,
+        countryCode: String
+    ): FetchedResource<List<SearchResult.TrackSearchResult>, MusifyErrorType> =
+        tokenRepository.runCatchingWithToken {
+            spotifyService.getTracksForGenre(
+                genre = genre.genreType.toSupportedSpotifyGenreType(),
+                market = countryCode,
+                token = it
+            ).value.map { trackDTOWithAlbumMetadata ->
+                trackDTOWithAlbumMetadata.toTrackSearchResult(imageSize)
+            }
+        }
+
+    override suspend fun fetchTracksForAlbumWithId(
+        albumId: String,
+        countryCode: String,
+        imageSize: MapperImageSize
+    ): FetchedResource<List<SearchResult.TrackSearchResult>, MusifyErrorType> =
+        tokenRepository.runCatchingWithToken {
+            spotifyService.getAlbumWithId(albumId, countryCode, it).getTracks(imageSize)
+        }
+}
