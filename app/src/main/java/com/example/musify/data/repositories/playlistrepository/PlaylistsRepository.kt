5c098be447fa1ffe071871a41352dd4a11d8c5f4
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-08-27 12:21:27 +0530

    refactor(PlaylistsRepository)!: replace use of PlaylistSummary with PlaylistSearchResult

--- app/src/main/java/com/example/musify/data/repositories/playlistrepository/PlaylistsRepository.kt
+++ app/src/main/java/com/example/musify/data/repositories/playlistrepository/PlaylistsRepository.kt
@@ -1,12 +1,12 @@
 package com.example.musify.data.repositories.playlistrepository
 
 import com.example.musify.data.utils.FetchedResource
-import com.example.musify.domain.MusicSummary
 import com.example.musify.domain.MusifyErrorType
+import com.example.musify.domain.SearchResult
 
 interface PlaylistsRepository {
     suspend fun fetchPlaylistWithId(
         playlistId: String,
         countryCode: String
-    ): FetchedResource<MusicSummary.PlaylistSummary, MusifyErrorType>
+    ): FetchedResource<SearchResult.PlaylistSearchResult, MusifyErrorType>
 }
