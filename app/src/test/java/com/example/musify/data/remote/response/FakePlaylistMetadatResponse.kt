5d80b944708fd1b7ffb9975bbf6146fabdc9eb74
Marsh Royden <marshroyden@Marshs-MacBook-Air.local>
2022-08-29 16:12:51 +0530

    test: add ownerName & totalNumber of tracks arg to fakePlaylistMetadataResponse top level prop

--- app/src/test/java/com/example/musify/data/remote/response/FakePlaylistMetadatResponse.kt
+++ app/src/test/java/com/example/musify/data/remote/response/FakePlaylistMetadatResponse.kt
@@ -3,6 +3,7 @@ package com.example.musify.data.remote.response
 val fakePlaylistMetadataResponse = PlaylistMetadataResponse(
     id = "testPlaylistId",
     name = "testPlaylistName",
-    images = List(3){fakeImageResponse},
-    ownerName = PlaylistResponse.OwnerNameWrapper("testOwnerName")
+    images = List(3) { fakeImageResponse },
+    ownerName = PlaylistMetadataResponse.OwnerNameWrapper("testOwnerName"),
+    totalNumberOfTracks = PlaylistMetadataResponse.TotalNumberOfTracksWrapper(0)
 )
