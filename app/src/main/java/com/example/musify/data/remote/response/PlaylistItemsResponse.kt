68a075593c9b21c035ead0e62ef6719e5fb59088
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-08-29 12:10:51 +0530

    refactor(PlaylistItemsResponse): add nested TrackResponseWithAlbumMetadataWrapper class & use it for 'items' property

--- app/src/main/java/com/example/musify/data/remote/response/PlaylistItemsResponse.kt
+++ app/src/main/java/com/example/musify/data/remote/response/PlaylistItemsResponse.kt
@@ -1,9 +1,15 @@
 package com.example.musify.data.remote.response
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 /**
  * A response class that contains a list of tracks in a particular
  * playlist.
  */
 data class PlaylistItemsResponse(
-    val items: List<PlaylistResponse.TrackResponseWithAlbumMetadataWrapper> // TODO move PlaylistResponse.TrackResponseWithAlbumMetadataWrapper into a nested class of this class
-)
+    val items: List<TrackResponseWithAlbumMetadataWrapper> // TODO remove PlaylistResponse.TrackResponseWithAlbumMetadataWrapper
+) {
+    data class TrackResponseWithAlbumMetadataWrapper(@JsonProperty("track") val track: TrackResponseWithAlbumMetadata)
+}
+
+
