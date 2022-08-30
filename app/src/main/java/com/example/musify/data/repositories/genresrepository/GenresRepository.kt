e7d97c2f831d2b9060fc0e253fb48cb91a0a661b
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-08-30 20:28:26 +0530

    docs(GenresRepository): add doc explaining about the interface

--- app/src/main/java/com/example/musify/data/repositories/genresrepository/GenresRepository.kt
+++ app/src/main/java/com/example/musify/data/repositories/genresrepository/GenresRepository.kt
@@ -2,6 +2,9 @@ package com.example.musify.data.repositories.genresrepository
 
 import com.example.musify.domain.Genre
 
+/**
+ * A repository that contains all methods related to genres.
+ */
 interface GenresRepository {
     fun fetchAvailableGenres(): List<Genre>
 }
