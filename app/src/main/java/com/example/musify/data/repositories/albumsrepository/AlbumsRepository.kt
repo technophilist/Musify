c3de560738fea5e6f8bdb263944fb0c0fdcdc0fb
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-08-30 20:33:29 +0530

    docs(AlbumsRepository): add doc explaining the interface

--- app/src/main/java/com/example/musify/data/repositories/albumsrepository/AlbumsRepository.kt
+++ app/src/main/java/com/example/musify/data/repositories/albumsrepository/AlbumsRepository.kt
@@ -7,6 +7,14 @@ import com.example.musify.domain.MusifyErrorType
 import com.example.musify.domain.SearchResult
 import kotlinx.coroutines.flow.Flow
 
+/**
+ * A repository that contains methods related to albums. **All methods
+ * of this interface will always return an instance of [SearchResult.AlbumSearchResult]**
+ * encapsulated inside [FetchedResource.Success] if the resource was
+ * fetched successfully. This ensures that the return value of all the
+ * methods of [AlbumsRepository] will always return [SearchResult.AlbumSearchResult]
+ * in the case of a successful fetch operation.
+ */
 interface AlbumsRepository {
     suspend fun fetchAlbumWithId(
         albumId: String,
