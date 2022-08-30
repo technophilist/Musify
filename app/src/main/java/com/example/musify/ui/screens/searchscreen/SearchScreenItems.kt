05ab258778d913494caa3d5c3595d9f207f47e16
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-08-30 13:05:57 +0530

    feat(SearchScreen): display message when search results are empty

--- app/src/main/java/com/example/musify/ui/screens/searchscreen/SearchScreenItems.kt
+++ app/src/main/java/com/example/musify/ui/screens/searchscreen/SearchScreenItems.kt
@@ -1,8 +1,19 @@
 package com.example.musify.ui.screens.searchscreen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyItemScope
 import androidx.compose.foundation.lazy.LazyListScope
-import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.itemsIndexed
 import com.example.musify.domain.SearchResult
@@ -10,6 +21,7 @@ import com.example.musify.ui.components.ListItemCardType
 import com.example.musify.ui.components.MusifyCompactListItemCard
 import com.example.musify.ui.components.MusifyCompactTrackCard
 import com.example.musify.ui.components.MusifyCompactTrackCardDefaults
+import com.google.accompanist.insets.imePadding
 
 @ExperimentalMaterialApi
 fun LazyListScope.searchTrackListItems(
@@ -20,7 +32,7 @@ fun LazyListScope.searchTrackListItems(
     onImageLoading: (SearchResult) -> Unit,
     onImageLoadingFinished: (SearchResult.TrackSearchResult, Throwable?) -> Unit
 ) {
-    itemsIndexed(
+    itemsIndexedWithEmptyListContent(
         items = tracksListForSearchQuery,
         key = { index, track -> "$index${track.id}" }
     ) { _, track ->
@@ -46,7 +58,7 @@ fun LazyListScope.searchAlbumListItems(
     onImageLoadingFinished: (SearchResult.AlbumSearchResult, Throwable?) -> Unit
 ) {
 
-    itemsIndexed(
+    itemsIndexedWithEmptyListContent(
         items = albumListForSearchQuery,
         key = { index, album -> "$index${album.id}" }
     ) { _, album ->
@@ -78,7 +90,7 @@ fun LazyListScope.searchArtistListItems(
     onImageLoadingFinished: (SearchResult.ArtistSearchResult, Throwable?) -> Unit,
     artistImageErrorPainter: Painter
 ) {
-    itemsIndexed(
+    itemsIndexedWithEmptyListContent(
         items = artistListForSearchQuery,
         key = { index, artist -> "$index${artist.id}" }
     ) { _, artist ->
@@ -111,7 +123,7 @@ fun LazyListScope.searchPlaylistListItems(
     onImageLoadingFinished: (SearchResult.PlaylistSearchResult, Throwable?) -> Unit,
     playlistImageErrorPainter: Painter
 ) {
-    itemsIndexed(
+    itemsIndexedWithEmptyListContent(
         items = playlistListForSearchQuery,
         key = { index, playlist -> "$index${playlist.id}" }
     ) { _, playlist ->
@@ -132,5 +144,49 @@ fun LazyListScope.searchPlaylistListItems(
                 contentPadding = MusifyCompactTrackCardDefaults.defaultContentPadding
             )
         }
+    }
+}
+
+@Composable
+private fun DefaultEmptyListContent(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Couldn't find anything matching the search query",
+            fontWeight = FontWeight.Bold,
+            style = MaterialTheme.typography.h6,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = "Try searching again using a different spelling or keyword.",
+            style = LocalTextStyle.current.copy(
+                color = Color.White.copy(alpha = ContentAlpha.disabled)
+            ),
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+private fun <T : Any> LazyListScope.itemsIndexedWithEmptyListContent(
+    items: LazyPagingItems<T>,
+    key: ((index: Int, item: T) -> Any)? = null,
+    emptyListContent: @Composable LazyItemScope.() -> Unit = {
+        DefaultEmptyListContent(
+            modifier = Modifier
+                .fillParentMaxSize()
+                .padding(horizontal = 16.dp)
+                .imePadding()
+        )
+    },
+    itemContent: @Composable LazyItemScope.(index: Int, value: T?) -> Unit
+) {
+    // items.loadState.append.endOfPaginationReached && items.itemCount == 0
+    if (true) {
+        item { emptyListContent.invoke(this) }
+    } else {
+        itemsIndexed(items, key, itemContent)
     }
 }
