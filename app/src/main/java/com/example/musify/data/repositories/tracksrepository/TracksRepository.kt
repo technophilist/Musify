d46ff81254f9b81fdc401134d6925ad13925de38
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-08-30 20:32:05 +0530

    docs(TracksRepository): add doc explaining the interface

--- app/src/main/java/com/example/musify/data/repositories/tracksrepository/TracksRepository.kt
+++ app/src/main/java/com/example/musify/data/repositories/tracksrepository/TracksRepository.kt
@@ -8,6 +8,14 @@ import com.example.musify.domain.MusifyErrorType
 import com.example.musify.domain.SearchResult
 import kotlinx.coroutines.flow.Flow
 
+/**
+ * A repository that contains methods related to tracks. **All methods
+ * of this interface will always return an instance of [SearchResult.TrackSearchResult]**
+ * encapsulated inside [FetchedResource.Success] if the resource was
+ * fetched successfully. This ensures that the return value of all the
+ * methods of [TracksRepository] will always return [SearchResult.TrackSearchResult]
+ * in the case of a successful fetch operation.
+ */
 interface TracksRepository {
     suspend fun fetchTopTenTracksForArtistWithId(
         artistId: String,
