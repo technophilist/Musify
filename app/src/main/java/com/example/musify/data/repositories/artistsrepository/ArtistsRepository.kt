fee5ba5d2a0384620af0a517e98deb020c92bc91
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-08-30 20:32:59 +0530

    docs(ArtistsRepository): add doc explaining the interface

--- app/src/main/java/com/example/musify/data/repositories/artistsrepository/ArtistsRepository.kt
+++ app/src/main/java/com/example/musify/data/repositories/artistsrepository/ArtistsRepository.kt
@@ -5,6 +5,14 @@ import com.example.musify.data.utils.MapperImageSize
 import com.example.musify.domain.MusifyErrorType
 import com.example.musify.domain.SearchResult
 
+/**
+ * A repository that contains methods related to artists. **All methods
+ * of this interface will always return an instance of [SearchResult.ArtistSearchResult]**
+ * encapsulated inside [FetchedResource.Success] if the resource was
+ * fetched successfully. This ensures that the return value of all the
+ * methods of [ArtistsRepository] will always return [SearchResult.ArtistSearchResult]
+ * in the case of a successful fetch operation.
+ */
 interface ArtistsRepository {
     suspend fun fetchArtistSummaryForId(
         artistId: String,
