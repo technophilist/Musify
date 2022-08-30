165ce5d9c720199c3408ebcd96b11966a0aaea65
Marsh Royden <marshroyden@Marshs-MacBook-Air.local>
2022-08-30 13:01:46 +0530

    fix(MainActivity): set soft input mode to SOFT_INPUT_ADJUST_RESIZE
    Setting this will enable the accompanist library to set the ime
    padding

--- app/src/main/java/com/example/musify/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/musify/ui/activities/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.musify.ui.activities
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
@@ -28,6 +29,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
         setContent {
             MusifyTheme {
                 ProvideWindowInsets {
