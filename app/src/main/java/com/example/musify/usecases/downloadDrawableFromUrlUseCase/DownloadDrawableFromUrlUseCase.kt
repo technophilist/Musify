ef8d46b81c5c36d6b554d633b6e319136952f326
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-09-01 13:56:35 +0530

    refactor(DownloadDrawableForUrlUseCase)!: replace 'application' param with 'context'

--- app/src/main/java/com/example/musify/usecases/downloadDrawableFromUrlUseCase/DownloadDrawableFromUrlUseCase.kt
+++ app/src/main/java/com/example/musify/usecases/downloadDrawableFromUrlUseCase/DownloadDrawableFromUrlUseCase.kt
@@ -1,11 +1,11 @@
 package com.example.musify.usecases.downloadDrawableFromUrlUseCase
 
-import android.app.Application
+import android.content.Context
 import android.graphics.drawable.Drawable
 
 fun interface DownloadDrawableFromUrlUseCase {
     suspend fun invoke(
         urlString: String,
-        application: Application
+        context: Context
     ): Result<Drawable>
 }
