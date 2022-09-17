458a5a9f8a0d139815ebec2b5308e690cf4a7556
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-09-17 13:16:40 +0530

    refactor(MusifyBackgroundMusicPlayer): use onPlaybackStateChanged && onPlayerError of exoplayer listener callbacks

--- app/src/main/java/com/example/musify/musicplayer/MusifyBackgroundMusicPlayer.kt
+++ app/src/main/java/com/example/musify/musicplayer/MusifyBackgroundMusicPlayer.kt
@@ -5,6 +5,7 @@ import com.example.musify.R
 import com.example.musify.musicplayer.utils.MediaDescriptionAdapter
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.PlaybackException
 import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.ui.PlayerNotificationManager
 import com.google.android.exoplayer2.util.NotificationUtil
@@ -13,19 +14,18 @@ import javax.inject.Inject
 
 
 class MusifyBackgroundMusicPlayer @Inject constructor(
-    @ApplicationContext context: Context,
-    private val exoPlayer: ExoPlayer
+    @ApplicationContext context: Context, private val exoPlayer: ExoPlayer
 ) : MusicPlayer {
     private var currentlyPlayingTrack: MusicPlayer.Track? = null
     private val notificationManagerBuilder by lazy {
         PlayerNotificationManager.Builder(context, NOTIFICATION_ID, NOTIFICATION_CHANNEL_ID)
-            .setChannelImportance(NotificationUtil.IMPORTANCE_LOW)
-            .setMediaDescriptionAdapter(MediaDescriptionAdapter(
-                getCurrentContentText = { currentlyPlayingTrack?.artistsString ?: "" },
-                getCurrentContentTitle = { currentlyPlayingTrack?.title ?: "" },
-                getCurrentLargeIcon = { _, _ -> currentlyPlayingTrack?.albumArt }
-            ))
-            .setChannelNameResourceId(R.string.notification_channel_name)
+            .setChannelImportance(NotificationUtil.IMPORTANCE_LOW).setMediaDescriptionAdapter(
+                MediaDescriptionAdapter(getCurrentContentText = {
+                    currentlyPlayingTrack?.artistsString ?: ""
+                },
+                    getCurrentContentTitle = { currentlyPlayingTrack?.title ?: "" },
+                    getCurrentLargeIcon = { _, _ -> currentlyPlayingTrack?.albumArt })
+            ).setChannelNameResourceId(R.string.notification_channel_name)
             .setChannelDescriptionResourceId(R.string.notification_channel_description)
     }
     private var listener: Player.Listener? = null
@@ -51,18 +51,26 @@ class MusifyBackgroundMusicPlayer @Inject constructor(
 
     override fun addOnPlaybackStateChangedListener(onPlaybackStateChanged: (MusicPlayer.PlaybackState) -> Unit) {
         listener = object : Player.Listener {
-            override fun onIsPlayingChanged(isPlaying: Boolean) {
-                when {
-                    exoPlayer.playerError != null -> onPlaybackStateChanged(MusicPlayer.PlaybackState.Error)
-                    isPlaying -> {
-                        currentlyPlayingTrack?.let {
-                            onPlaybackStateChanged(MusicPlayer.PlaybackState.Playing(it))
-                        }
+            override fun onPlaybackStateChanged(playbackState: Int) {
+                super.onPlaybackStateChanged(playbackState)
+                when (playbackState) {
+                    Player.STATE_IDLE -> onPlaybackStateChanged(MusicPlayer.PlaybackState.Idle)
+                    Player.STATE_BUFFERING -> {}
+                    Player.STATE_ENDED -> {}
+                    Player.STATE_READY -> {
+                        if (!exoPlayer.playWhenReady) onPlaybackStateChanged(MusicPlayer.PlaybackState.Paused)
                     }
-                    !isPlaying && !exoPlayer.playWhenReady -> onPlaybackStateChanged(MusicPlayer.PlaybackState.Paused)
-                    !isPlaying && (exoPlayer.playbackState == ExoPlayer.STATE_ENDED) ->
-                        onPlaybackStateChanged(MusicPlayer.PlaybackState.Stopped)
                 }
+            }
+
+            override fun onPlayerError(error: PlaybackException) {
+                onPlaybackStateChanged(MusicPlayer.PlaybackState.Error)
+            }
+
+            override fun onIsPlayingChanged(isPlaying: Boolean) {
+                if (!isPlaying) return
+                val state = currentlyPlayingTrack?.let(MusicPlayer.PlaybackState::Playing) ?: return
+                onPlaybackStateChanged(state)
             }
         }
         exoPlayer.addListener(listener!!)
