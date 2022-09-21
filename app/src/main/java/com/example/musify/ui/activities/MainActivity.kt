c10626c1769ccdc393a678764c4732a5400904b6
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-09-21 15:54:51 +0530

    feat: make NowPlayingScreen visible when mini player is clicked

--- app/src/main/java/com/example/musify/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/musify/ui/activities/MainActivity.kt
@@ -5,7 +5,9 @@ import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.*
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -19,6 +21,7 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.musify.ui.components.MusifyMiniPlayer
 import com.example.musify.ui.screens.MusifyNavigation
+import com.example.musify.ui.screens.NowPlayingScreen
 import com.example.musify.ui.theme.MusifyTheme
 import com.example.musify.viewmodels.PlaybackViewModel
 import com.google.accompanist.insets.ProvideWindowInsets
@@ -61,6 +64,7 @@ private fun MusifyApp() {
     val miniPlayerTrack = remember(playbackState) {
         playbackState.currentlyPlayingTrack ?: playbackState.previouslyPlayingTrack
     }
+    var isNowPlayingScreenVisible by remember { mutableStateOf(false) }
     LaunchedEffect(key1 = playbackEvent) {
         if (playbackEvent !is PlaybackViewModel.Event.PlaybackError) return@LaunchedEffect
         snackbarHostState.currentSnackbarData?.dismiss()
@@ -87,32 +91,48 @@ private fun MusifyApp() {
             modifier = Modifier
                 .align(Alignment.BottomCenter)
                 .fillMaxWidth()
-                .navigationBarsPadding()
-                .padding(horizontal = 8.dp)
-                .padding(bottom = 8.dp),
+                .animateContentSize(animationSpec = tween()),
             visible = playbackState.currentlyPlayingTrack != null || playbackState.previouslyPlayingTrack != null,
             enter = fadeIn() + slideInVertically { it },
             exit = fadeOut() + slideOutVertically { -it },
         ) {
             miniPlayerTrack?.let {
-                MusifyMiniPlayer(
-                    isPlaybackPaused = playbackState is PlaybackViewModel.PlaybackState.Paused || playbackState is PlaybackViewModel.PlaybackState.PlaybackEnded,
-                    currentlyPlayingTrack = it,
-                    onLikedButtonClicked = {},
-                    onPlayButtonClicked = {
-                        if (playbackState is PlaybackViewModel.PlaybackState.Paused) {
-                            playbackViewModel.resumePlaybackIfPaused()
-                            return@MusifyMiniPlayer
-                        }
-                        if (playbackState is PlaybackViewModel.PlaybackState.PlaybackEnded) {
-                            // play the same track again
-                            playbackViewModel.playTrack(it)
-                        }
-                    },
-                    onPauseButtonClicked = playbackViewModel::pauseCurrentlyPlayingTrack
-                )
+                if (isNowPlayingScreenVisible) {
+                    NowPlayingScreen(currentlyPlayingTrack = it,
+                        playbackDurationRange = 0f..1f, // TODO
+                        playbackProgressProvider = { 0.5f },
+                        onCloseButtonClicked = { isNowPlayingScreenVisible = false },
+                        onShuffleButtonClicked = { /*TODO*/ },
+                        onSkipPreviousButtonClicked = { /*TODO*/ },
+                        onPlayButtonClicked = { /*TODO*/ },
+                        onPauseButtonClicked = { /*TODO*/ },
+                        onSkipNextButtonClicked = { /*TODO*/ }) {}
+                } else {
+                    MusifyMiniPlayer(
+                        modifier = Modifier
+                            .navigationBarsPadding()
+                            .padding(horizontal = 8.dp)
+                            .padding(bottom = 8.dp)
+                            .clickable { isNowPlayingScreenVisible = true },
+                        isPlaybackPaused = playbackState is PlaybackViewModel.PlaybackState.Paused || playbackState is PlaybackViewModel.PlaybackState.PlaybackEnded,
+                        currentlyPlayingTrack = it,
+                        onLikedButtonClicked = {},
+                        onPlayButtonClicked = {
+                            if (playbackState is PlaybackViewModel.PlaybackState.Paused) {
+                                playbackViewModel.resumePlaybackIfPaused()
+                                return@MusifyMiniPlayer
+                            }
+                            if (playbackState is PlaybackViewModel.PlaybackState.PlaybackEnded) {
+                                // play the same track again
+                                playbackViewModel.playTrack(it)
+                            }
+                        },
+                        onPauseButtonClicked = playbackViewModel::pauseCurrentlyPlayingTrack
+                    )
+                }
             }
         }
     }
 }
+
 
