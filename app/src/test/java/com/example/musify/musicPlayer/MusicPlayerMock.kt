f67eb09667229eaf649de8d841e2e0b100b53859
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-09-29 11:40:56 +0530

    fix(MusicPlayerMock): accomodate for changes in the MusicPlayer interface

--- app/src/test/java/com/example/musify/musicPlayer/MusicPlayerMock.kt
+++ app/src/test/java/com/example/musify/musicPlayer/MusicPlayerMock.kt
@@ -1,20 +1,45 @@
 package com.example.musify.musicPlayer
 
 import com.example.musify.musicplayer.MusicPlayer
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.flow
 
 class MusicPlayerMock : MusicPlayer {
     private lateinit var onPlaybackStateChangedCallback: (MusicPlayer.PlaybackState) -> Unit
-
+    private var currentlyPlayingTrack: MusicPlayer.Track? = null
+    private var isPlaybackPaused = false
     override fun pauseCurrentlyPlayingTrack() {
-        onPlaybackStateChangedCallback(MusicPlayer.PlaybackState.Paused)
+        val pausedState = currentlyPlayingTrack?.let(MusicPlayer.PlaybackState::Paused) ?: return
+        onPlaybackStateChangedCallback(pausedState)
     }
 
     override fun stopPlayingTrack() {
-        onPlaybackStateChangedCallback(MusicPlayer.PlaybackState.Stopped)
+        onPlaybackStateChangedCallback(MusicPlayer.PlaybackState.Idle)
     }
 
     override fun playTrack(track: MusicPlayer.Track) {
-        onPlaybackStateChangedCallback(MusicPlayer.PlaybackState.Playing(track))
+        if (currentlyPlayingTrack == track) {
+            return
+        }
+        currentlyPlayingTrack = track
+        onPlaybackStateChangedCallback(
+            MusicPlayer.PlaybackState.Playing(
+                currentlyPlayingTrack = track,
+                totalDuration = 5_000,
+                currentPlaybackPositionInMillisFlow = flow {
+                    for (i in 1..5) {
+                        emit(i * 1_000L)
+                        while (isPlaybackPaused) emit(i * 1000L)
+                        delay(1_000)
+                    }
+                }.distinctUntilChanged()
+            )
+        )
+    }
+
+    override fun tryResume(): Boolean {
+        return currentlyPlayingTrack != null
     }
 
     override fun addOnPlaybackStateChangedListener(onPlaybackStateChanged: (MusicPlayer.PlaybackState) -> Unit) {
