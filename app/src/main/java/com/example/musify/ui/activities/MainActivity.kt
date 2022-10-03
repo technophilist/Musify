14b10ab69eb8dea28ffc5b18be66835cbe64e164
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-10-03 12:47:10 +0530

    feat(MainActivity): add bottom navigation bar

--- app/src/main/java/com/example/musify/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/musify/ui/activities/MainActivity.kt
@@ -18,7 +18,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.musify.domain.SearchResult
+import com.example.musify.ui.components.MusifyBottomNavigation
 import com.example.musify.ui.components.MusifyMiniPlayer
+import com.example.musify.ui.navigation.MusifyBottomNavigationDestinations
 import com.example.musify.ui.screens.MusifyNavigation
 import com.example.musify.ui.screens.NowPlayingScreen
 import com.example.musify.ui.theme.MusifyTheme
@@ -36,7 +38,8 @@ class MainActivity : ComponentActivity() {
         window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
         setContent {
             MusifyTheme {
-                Surface(modifier = Modifier.fillMaxSize(),
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background,
                     content = { MusifyApp() }
                 )
@@ -83,6 +86,16 @@ private fun MusifyApp() {
     BackHandler(isNowPlayingScreenVisible) {
         isNowPlayingScreenVisible = false
     }
+    val bottomNavigationItems = remember {
+        listOf(
+            MusifyBottomNavigationDestinations.Home,
+            MusifyBottomNavigationDestinations.Search,
+            MusifyBottomNavigationDestinations.Premium
+        )
+    }
+    var currentlySelectedBottomNavigationItem by remember {
+        mutableStateOf<MusifyBottomNavigationDestinations>(MusifyBottomNavigationDestinations.Search)
+    }
     Box(modifier = Modifier.fillMaxSize()) {
         // the playbackState.currentlyPlayingTrack will automatically be set
         // to null when the playback is stopped
@@ -92,63 +105,65 @@ private fun MusifyApp() {
             isPlaybackLoading = playbackState is PlaybackViewModel.PlaybackState.Loading,
             isFullScreenNowPlayingOverlayScreenVisible = isNowPlayingScreenVisible
         )
-        SnackbarHost(
+        Column(
             modifier = Modifier
+                .fillMaxWidth()
                 .align(Alignment.BottomCenter)
-                .navigationBarsPadding()
-                .padding(8.dp),
-            hostState = snackbarHostState
-        )
-        AnimatedVisibility(
-            modifier = Modifier
-                .fillMaxWidth()
-                .align(Alignment.BottomCenter),
-            visible = playbackState.currentlyPlayingTrack != null || playbackState.previouslyPlayingTrack != null,
-            enter = fadeIn() + slideInVertically { it },
-            exit = fadeOut() + slideOutVertically { -it },
         ) {
-            AnimatedContent(modifier = Modifier
-                .fillMaxWidth()
-                .align(Alignment.BottomCenter),
-                targetState = isNowPlayingScreenVisible,
-                transitionSpec = {
-                    slideInVertically(animationSpec = tween()) with shrinkOut(animationSpec = tween())
-                }
-            ) { isFullScreenVisible ->
-                miniPlayerTrack?.let {
-                    if (isFullScreenVisible) {
-                        NowPlayingScreen(
-                            currentlyPlayingTrack = it,
-                            isPlaybackPaused = isPlaybackPaused,
-                            timeElapsedStringFlow = playbackViewModel.flowOfProgressTextOfCurrentTrack.value,
-                            totalDurationOfCurrentTrackProvider = { totalDurationOfCurrentTrackText },
-                            playbackDurationRange = PlaybackViewModel.PLAYBACK_PROGRESS_RANGE,
-                            playbackProgressFlow = playbackViewModel.flowOfProgressOfCurrentTrack.value,
-                            onCloseButtonClicked = { isNowPlayingScreenVisible = false },
-                            onShuffleButtonClicked = { /*TODO*/ },
-                            onSkipPreviousButtonClicked = { /*TODO*/ },
-                            onPlayButtonClicked = { onPlayButtonClicked(miniPlayerTrack) },
-                            onPauseButtonClicked = playbackViewModel::pauseCurrentlyPlayingTrack,
-                            onSkipNextButtonClicked = { /*TODO*/ },
-                            onRepeatButtonClicked = {}
-                        )
-                    } else {
-                        MusifyMiniPlayer(
-                            modifier = Modifier
-                                .navigationBarsPadding()
-                                .padding(horizontal = 8.dp)
-                                .padding(bottom = 8.dp)
-                                .clickable { isNowPlayingScreenVisible = true },
-                            isPlaybackPaused = isPlaybackPaused,
-                            currentlyPlayingTrack = it,
-                            onLikedButtonClicked = {},
-                            onPlayButtonClicked = { onPlayButtonClicked(miniPlayerTrack) },
-                            onPauseButtonClicked = playbackViewModel::pauseCurrentlyPlayingTrack
-                        )
+            SnackbarHost(
+                modifier = Modifier.padding(8.dp),
+                hostState = snackbarHostState
+            )
+            androidx.compose.animation.AnimatedVisibility(
+                visible = playbackState.currentlyPlayingTrack != null || playbackState.previouslyPlayingTrack != null,
+                enter = fadeIn() + slideInVertically { it },
+                exit = fadeOut() + slideOutVertically { -it },
+            ) {
+                AnimatedContent(modifier = Modifier.fillMaxWidth(),
+                    targetState = isNowPlayingScreenVisible,
+                    transitionSpec = {
+                        slideInVertically(animationSpec = tween()) with shrinkOut(animationSpec = tween())
                     }
+                ) { isFullScreenVisible ->
+                    miniPlayerTrack?.let {
+                        if (isFullScreenVisible) {
+                            NowPlayingScreen(
+                                currentlyPlayingTrack = it,
+                                isPlaybackPaused = isPlaybackPaused,
+                                timeElapsedStringFlow = playbackViewModel.flowOfProgressTextOfCurrentTrack.value,
+                                totalDurationOfCurrentTrackProvider = { totalDurationOfCurrentTrackText },
+                                playbackDurationRange = PlaybackViewModel.PLAYBACK_PROGRESS_RANGE,
+                                playbackProgressFlow = playbackViewModel.flowOfProgressOfCurrentTrack.value,
+                                onCloseButtonClicked = { isNowPlayingScreenVisible = false },
+                                onShuffleButtonClicked = { /*TODO*/ },
+                                onSkipPreviousButtonClicked = { /*TODO*/ },
+                                onPlayButtonClicked = { onPlayButtonClicked(miniPlayerTrack) },
+                                onPauseButtonClicked = playbackViewModel::pauseCurrentlyPlayingTrack,
+                                onSkipNextButtonClicked = { /*TODO*/ },
+                                onRepeatButtonClicked = {}
+                            )
+                        } else {
+                            MusifyMiniPlayer(
+                                modifier = Modifier
+                                    .padding(horizontal = 8.dp)
+                                    .clickable { isNowPlayingScreenVisible = true },
+                                isPlaybackPaused = isPlaybackPaused,
+                                currentlyPlayingTrack = it,
+                                onLikedButtonClicked = {},
+                                onPlayButtonClicked = { onPlayButtonClicked(miniPlayerTrack) },
+                                onPauseButtonClicked = playbackViewModel::pauseCurrentlyPlayingTrack
+                            )
+                        }
 
+                    }
                 }
             }
+            MusifyBottomNavigation(
+                modifier = Modifier.navigationBarsPadding(),
+                navigationItems = bottomNavigationItems,
+                currentlySelectedItem = currentlySelectedBottomNavigationItem,
+                onItemClick = { currentlySelectedBottomNavigationItem = it }
+            )
         }
     }
 }
