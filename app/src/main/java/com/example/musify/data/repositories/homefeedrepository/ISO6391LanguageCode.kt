a9b16ff99ec163275db02d86550e458ba559ff63
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-10-06 12:36:11 +0530

    refactor: add ISO6391LanguageCode value class

---
+++ app/src/main/java/com/example/musify/data/repositories/homefeedrepository/ISO6391LanguageCode.kt
@@ -0,0 +1,9 @@
+package com.example.musify.data.repositories.homefeedrepository
+
+/**
+ * A wrapper class that wraps a string that contains a language
+ * code conforming to the ISO-639-1 language code format.
+ */
+@JvmInline
+value class ISO6391LanguageCode(val value: String)
+
