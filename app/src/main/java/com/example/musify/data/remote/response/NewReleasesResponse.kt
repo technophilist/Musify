a2d2a0791aab80524c052e6c17a1b4aeb2fd712d
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-10-06 12:31:26 +0530

    refactor(NewReleasesResponse): add mapper method to convert instance to a list of AlbumSearchResults

--- app/src/main/java/com/example/musify/data/remote/response/NewReleasesResponse.kt
+++ app/src/main/java/com/example/musify/data/remote/response/NewReleasesResponse.kt
@@ -1,5 +1,8 @@
 package com.example.musify.data.remote.response
 
+import com.example.musify.data.utils.MapperImageSize
+import com.example.musify.domain.SearchResult
+
 /**
  * A response object that contains [albums] that were newly released.
  */
@@ -9,3 +12,10 @@ data class NewReleasesResponse(val albums: Albums) {
      */
     data class Albums(val items: List<AlbumMetadataResponse>)
 }
+
+/**
+ * A mapper method used to map an instance of [NewReleasesResponse] to
+ * a list of [SearchResult.AlbumSearchResult].
+ */
+fun NewReleasesResponse.toAlbumSearchResultList(size: MapperImageSize): List<SearchResult.AlbumSearchResult> =
+    this.albums.items.map { it.toAlbumSearchResult(size) }
