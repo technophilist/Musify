d6959dceb01a2d5c79a1379322a5364c205a2507
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-10-08 22:16:59 +0530

    test(SpotifyPagingSourceTest): use mockito instead of manually creating a stub for SpotifyService

--- app/src/test/java/com/example/musify/data/paging/SpotifySearchPagingSourceTest.kt
+++ app/src/test/java/com/example/musify/data/paging/SpotifySearchPagingSourceTest.kt
@@ -3,32 +3,46 @@ package com.example.musify.data.paging
 import androidx.paging.PagingSource
 import com.example.musify.data.remote.musicservice.SearchQueryType
 import com.example.musify.data.remote.musicservice.SpotifyService
-import com.example.musify.data.remote.musicservice.SupportedSpotifyGenres
-import com.example.musify.data.remote.response.*
-import com.example.musify.data.remote.token.BearerToken
+import com.example.musify.data.remote.response.SearchResultsResponse
+import com.example.musify.data.remote.response.toSearchResults
 import com.example.musify.data.repositories.tokenrepository.TokenRepositoryStub
 import com.example.musify.data.utils.MapperImageSize
 import com.example.musify.domain.SearchResult
 import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Test
+import org.mockito.kotlin.any
+import org.mockito.kotlin.doAnswer
+import org.mockito.kotlin.mock
 import java.io.IOException
 
 
 private const val TEST_PAGE_SIZE = 2
 private const val PAGE_NUMBER_THAT_THROWS_IO_EXCEPTION = 100
 
+
 class SpotifySearchPagingSourceTest {
     private lateinit var pagingSource: SpotifyPagingSource<SearchResult.TrackSearchResult>
 
     @Before
     fun setUp() {
+        val spotifyServiceStub = mock<SpotifyService> {
+            onBlocking { search(any(), any(), any(), any(), any(), any()) } doAnswer {
+                when (it.arguments[4]) {
+                    PAGE_NUMBER_THAT_THROWS_IO_EXCEPTION * TEST_PAGE_SIZE -> throw IOException("")
+                    0 -> getSearchResultsResponseForFakePageNumber(FakeSearchResultsPageNumbers.PAGE_1)
+                    2 -> getSearchResultsResponseForFakePageNumber(FakeSearchResultsPageNumbers.PAGE_2)
+                    4 -> getSearchResultsResponseForFakePageNumber(FakeSearchResultsPageNumbers.PAGE_3)
+                    else -> SearchResultsResponse(null, null, null, null) // last page
+                }
+            }
+        }
         pagingSource = SpotifySearchPagingSource(
             searchQuery = "",
             countryCode = "",
             tokenRepository = TokenRepositoryStub(),
             searchQueryType = SearchQueryType.TRACK,
-            spotifyService = SpotifyServiceSearchStub(),
+            spotifyService = spotifyServiceStub,
             resultsBlock = { it.toSearchResults(MapperImageSize.SMALL).tracks }
         )
     }
@@ -102,112 +116,5 @@ class SpotifySearchPagingSourceTest {
         assert(loadResult is PagingSource.LoadResult.Error<Int, SearchResult.TrackSearchResult>)
         loadResult as PagingSource.LoadResult.Error
         assert(loadResult.throwable is IOException)
-    }
-}
-
-private class SpotifyServiceSearchStub : SpotifyService {
-    override suspend fun getTracksForPlaylist(
-        playlistId: String,
-        market: String,
-        token: BearerToken,
-        limit: Int,
-        offset: Int
-    ): PlaylistItemsResponse {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun getNewReleases(
-        token:BearerToken,
-        market: String,
-        limit: Int,
-        offset: Int
-    ): NewReleasesResponse {
-        TODO("Not yet implemented")
-    }
-
-
-    override suspend fun getArtistInfoWithId(
-        artistId: String,
-        token: BearerToken
-    ): ArtistResponse = TODO("Not yet implemented")
-
-
-    override suspend fun getAlbumsOfArtistWithId(
-        artistId: String,
-        market: String,
-        token: BearerToken,
-        limit: Int,
-        offset: Int,
-        includeGroups: String?
-    ): AlbumsMetadataResponse = TODO("Not yet implemented")
-
-
-    override suspend fun getTopTenTracksForArtistWithId(
-        artistId: String,
-        market: String,
-        token: BearerToken
-    ): TracksWithAlbumMetadataListResponse = TODO("Not yet implemented")
-
-
-    override suspend fun getAlbumWithId(
-        albumId: String,
-        market: String,
-        token: BearerToken
-    ): AlbumResponse = TODO("Not yet implemented")
-
-
-    override suspend fun search(
-        searchQuery: String,
-        market: String,
-        token: BearerToken,
-        limit: Int,
-        offset: Int,
-        type: String
-    ): SearchResultsResponse = when (offset) {
-        PAGE_NUMBER_THAT_THROWS_IO_EXCEPTION * TEST_PAGE_SIZE -> throw IOException("")
-        0 -> getSearchResultsResponseForFakePageNumber(FakeSearchResultsPageNumbers.PAGE_1)
-        2 -> getSearchResultsResponseForFakePageNumber(FakeSearchResultsPageNumbers.PAGE_2)
-        4 -> getSearchResultsResponseForFakePageNumber(FakeSearchResultsPageNumbers.PAGE_3)
-        else -> SearchResultsResponse(null, null, null, null) // last page
-    }
-
-    override suspend fun getTracksForGenre(
-        genre: SupportedSpotifyGenres,
-        market: String,
-        token: BearerToken,
-        limit: Int
-    ): TracksWithAlbumMetadataListResponse = TODO()
-
-    override suspend fun getFeaturedPlaylists(
-        token: BearerToken,
-        market: String,
-        locale: String,
-        timestamp: String,
-        limit: Int,
-        offset: Int
-    ): FeaturedPlaylistsResponse {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun getPlaylistsForCategory(
-        token: BearerToken,
-        categoryId: String,
-        market: String,
-        locale: String,
-        timestamp: String,
-        limit: Int,
-        offset: Int
-    ): PlaylistsForSpecificCategoryResponse {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun getBrowseCategories(
-        token: BearerToken,
-        market: String,
-        locale: String,
-        limit: Int,
-        offset: Int
-    ): BrowseCategoriesResponse {
-        TODO("Not yet implemented")
     }
 }
