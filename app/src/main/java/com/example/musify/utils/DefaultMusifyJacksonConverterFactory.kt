a2907c37e9c5af0d82fb462b96682fb9b37717dc
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-10-08 23:47:11 +0530

    refactor: enable strict null checks for collections in jackson mapper

--- app/src/main/java/com/example/musify/utils/DefaultMusifyJacksonConverterFactory.kt
+++ app/src/main/java/com/example/musify/utils/DefaultMusifyJacksonConverterFactory.kt
@@ -1,13 +1,24 @@
 package com.example.musify.utils
 
 import com.fasterxml.jackson.databind.DeserializationFeature
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinFeature
+import com.fasterxml.jackson.module.kotlin.jsonMapper
+import com.fasterxml.jackson.module.kotlin.kotlinModule
 import retrofit2.converter.jackson.JacksonConverterFactory
+
+/**
+ * A kotlin module with strict null checks enabled for collections.
+ * TODO: consider using strict null checks only during the debug version and not for the release.
+ */
+private val jacksonModule = kotlinModule {
+    configure(KotlinFeature.StrictNullChecks, true) // warning: this feature has a significant performance impact.
+}
 
 /**
  * A [JacksonConverterFactory] that is configured to not throw an
  * exception when an unknown json property is encountered.
  */
 val defaultMusifyJacksonConverterFactory: JacksonConverterFactory = JacksonConverterFactory.create(
-    jacksonObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+    jsonMapper { addModule(jacksonModule) }
+        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
 )
