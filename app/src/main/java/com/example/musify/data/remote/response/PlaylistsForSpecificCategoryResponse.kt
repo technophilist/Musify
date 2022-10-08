84ae54cebf6d5dadb972df71efdc9efdad57609d
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-10-09 00:09:42 +0530

    fix(PlaylistsForSpecificCategoryResponse): make jackson ignore null playlist in PlaylistsForSpecificCategoryResponseItems.items collection

--- app/src/main/java/com/example/musify/data/remote/response/PlaylistsForSpecificCategoryResponse.kt
+++ app/src/main/java/com/example/musify/data/remote/response/PlaylistsForSpecificCategoryResponse.kt
@@ -3,6 +3,8 @@ package com.example.musify.data.remote.response
 import com.example.musify.data.remote.response.PlaylistMetadataResponse.OwnerNameWrapper
 import com.example.musify.domain.SearchResult
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonSetter
+import com.fasterxml.jackson.annotation.Nulls
 
 /**
  * A response object that contains a list of [playlists] that are associated
@@ -12,13 +14,14 @@ data class PlaylistsForSpecificCategoryResponse(val playlists: PlaylistsForSpeci
     /**
      * A response class that contains a list of [PlaylistMetadataWithImageUrlListResponse].
      */
-    data class PlaylistsForSpecificCategoryResponseItems(val items: List<PlaylistMetadataWithImageUrlListResponse>)
+    data class PlaylistsForSpecificCategoryResponseItems(@JsonSetter(contentNulls = Nulls.SKIP) val items: List<PlaylistMetadataWithImageUrlListResponse>)
 
     /**
      * A response class that is very similar to [PlaylistMetadataResponse]. This class
      * only differs in the fact that it has [imageUrlList] instead of [PlaylistMetadataResponse.images]
      * since the dimension information will not be provided for [PlaylistsForSpecificCategoryResponse].
      */
+
     data class PlaylistMetadataWithImageUrlListResponse(
         val id: String,
         val name: String,
