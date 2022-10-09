6ee150adf708a5fde3a0ff7dacb1608077601d0e
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-10-09 16:09:37 +0530

    test: add TimeBasedGreetingPhraseGeneratorTest

---
+++ app/src/test/java/com/example/musify/viewmodels/homefeedviewmodel/greetingphrasegenerator/TimeBasedGreetingPhraseGeneratorTest.kt
@@ -0,0 +1,21 @@
+package com.example.musify.viewmodels.homefeedviewmodel.greetingphrasegenerator
+
+import org.junit.Test
+import java.time.LocalTime
+
+class TimeBasedGreetingPhraseGeneratorTest {
+
+    @Test
+    fun phraseGenerationTest_24hourIntRange_generatesValidPhrases() {
+        (0..23).forEach { hour ->
+            val phrase = TimeBasedGreetingPhraseGenerator(LocalTime.of(hour, 0)).generatePhrase()
+            assert(
+                phrase == when (hour) {
+                    in 0..11 -> "Good morning"
+                    in 12..15 -> "Good afternoon"
+                    else -> "Good evening"
+                }
+            )
+        }
+    }
+}
