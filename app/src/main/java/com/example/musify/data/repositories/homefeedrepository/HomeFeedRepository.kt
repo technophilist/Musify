f0fd74584ffb01a443219270ae9763abd650a7c4
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-10-09 11:57:18 +0530

    refactor: add HomeFeedRepository interface

---
+++ app/src/main/java/com/example/musify/data/repositories/homefeedrepository/HomeFeedRepository.kt
@@ -0,0 +1,31 @@
+package com.example.musify.data.repositories.homefeedrepository
+
+import com.example.musify.data.utils.FetchedResource
+import com.example.musify.data.utils.MapperImageSize
+import com.example.musify.domain.FeaturedPlaylists
+import com.example.musify.domain.MusifyErrorType
+import com.example.musify.domain.PlaylistsForCategory
+import com.example.musify.domain.SearchResult
+
+/**
+ * An interface that contains the requisite methods required for a repository
+ * that contains methods for fetching items to be displayed in the home
+ * feed.
+ */
+interface HomeFeedRepository {
+    suspend fun fetchFeaturedPlaylistsForCurrentTimeStamp(
+        timestampMillis: Long,
+        countryCode: String,
+        languageCode: ISO6391LanguageCode,
+    ): FetchedResource<FeaturedPlaylists, MusifyErrorType>
+
+    suspend fun fetchPlaylistsBasedOnCategoriesAvailableForCountry(
+        countryCode: String,
+        languageCode: ISO6391LanguageCode,
+    ): FetchedResource<List<PlaylistsForCategory>, MusifyErrorType>
+
+    suspend fun fetchNewlyReleasedAlbums(
+        countryCode: String,
+        mapperImageSize: MapperImageSize
+    ): FetchedResource<List<SearchResult.AlbumSearchResult>, MusifyErrorType>
+}
