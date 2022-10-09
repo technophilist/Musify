a4e41e8cde7dd09cfd73e6190f71ddb1f92f5caa
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-10-09 16:02:19 +0530

    refactor: add TimeBasedGreetingPhraseGenerator class

---
+++ app/src/main/java/com/example/musify/viewmodels/homefeedviewmodel/greetingphrasegenerator/TimeBasedGreetingPhraseGenerator.kt
@@ -0,0 +1,19 @@
+package com.example.musify.viewmodels.homefeedviewmodel.greetingphrasegenerator
+
+import java.time.LocalTime
+import javax.inject.Inject
+
+/**
+ * A concrete implementation of [GreetingPhraseGenerator] that generates
+ * greeting phrases based on the provided [time] value. By default the
+ * [time] property is set to [LocalTime.now].
+ */
+class TimeBasedGreetingPhraseGenerator @Inject constructor(
+    private val time: LocalTime = LocalTime.now()
+) : GreetingPhraseGenerator {
+    override fun generatePhrase(): String = when {
+        time.isMorning -> "Good morning"
+        time.isNoon -> "Good afternoon"
+        else -> "Good evening"
+    }
+}
