2eb85baeedafe161e4cc591d2870672e639339a2
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-10-09 16:00:57 +0530

    refactor: add GreetingPhraseGenerator interface

---
+++ app/src/main/java/com/example/musify/viewmodels/homefeedviewmodel/greetingphrasegenerator/GreetingPhraseGenerator.kt
@@ -0,0 +1,9 @@
+package com.example.musify.viewmodels.homefeedviewmodel.greetingphrasegenerator
+
+/**
+ * An interface that contains the requisite functions required for a
+ * class that is responsible for generating a greeting phrase.
+ */
+fun interface GreetingPhraseGenerator {
+    fun generatePhrase(): String
+}
