891846aaa3a084c9ef8664a3f3121b84cdb4b3e0
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-10-14 17:13:00 +0530

    refactor: add top-level "defaultPremiumPlans" list

--- app/src/main/java/com/example/musify/domain/PremiumPlanInformation.kt
+++ app/src/main/java/com/example/musify/domain/PremiumPlanInformation.kt
@@ -23,8 +23,8 @@ data class PremiumPlanInformation(
     val highlights: List<String>,
     val termsAndConditions: String,
     val pricingInformation: PricingInformation,
-    val darkColor:Color,
-    val lightColor:Color
+    val darkColor: Color,
+    val lightColor: Color
 ) {
     /**
      * A class that contains pricing information related to an instance of
@@ -40,3 +40,98 @@ data class PremiumPlanInformation(
         val term: String
     )
 }
+
+/**
+ * A list of premium plans that are available by default.
+ */
+val defaultPremiumPlans = listOf(
+    PremiumPlanInformation(
+        id = "premium_mini",
+        name = "Mini",
+        highlights = listOf(
+            "1 day and 1 week plans",
+            "Ad-free music on mobile",
+            "Download 30 songs on 1 mobile device",
+            "Mobile only plan"
+        ),
+        termsAndConditions = "Prices vary according to duration of plan. Terms and conditions apply.",
+        pricingInformation = PricingInformation(
+            associatedCardId = "premium_mini",
+            cost = "From $7",
+            term = "For 1 day"
+        ),
+        darkColor = Color(0xFF2F4ABC),
+        lightColor = Color(0xFF4F99F4)
+    ),
+    PremiumPlanInformation(
+        id = "premium_individual",
+        name = "Premium Individual",
+        highlights = listOf(
+            "Ad-free music listening",
+            "Download to listen offline",
+            "Debit and credit cards accepted"
+        ),
+        termsAndConditions = "Offer only for users who are new to Premium. Terms and conditions apply.",
+        pricingInformation = PricingInformation(
+            associatedCardId = "premium_individual",
+            cost = "Free",
+            term = "For 1 month"
+        ),
+        darkColor = Color(0xFF045746),
+        lightColor = Color(0xFF16A96A),
+    ),
+    PremiumPlanInformation(
+        id = "premium_duo",
+        name = "Premium Duo",
+        highlights = listOf(
+            "2 Premium accounts",
+            "For couples who live together",
+            "Ad-free music listening",
+            "Download 10,000 songs/device, on up to 5 devices per account",
+            "Choose 1,3,6 or 12 months of Premium",
+            "Debit and credit cards accepted"
+        ),
+        termsAndConditions = "Offer only for users who are new to Premium. Terms and conditions apply.",
+        pricingInformation = PricingInformation(
+            associatedCardId = "premium_duo",
+            cost = "Free",
+            term = "For 1 month"
+        ),
+        darkColor = Color(0xff3F3F76),
+        lightColor = Color(0xff5992C2),
+    ),
+    PremiumPlanInformation(
+        id = "premium_family",
+        name = "Premium Family",
+        highlights = listOf(
+            "Add-free music listening",
+            "Choose 1,3,6 or 12 months of Premium",
+            "Ad-free music listening",
+            "Debit and credit cards accepted"
+        ),
+        termsAndConditions = "Offer only for users who are new to Premium. Terms and conditions apply.",
+        pricingInformation = PricingInformation(
+            associatedCardId = "premium_family",
+            cost = "Free",
+            term = "For 1 month"
+        ),
+        darkColor = Color(0xFF213265),
+        lightColor = Color(0xFF972A8E)
+    ),
+    PremiumPlanInformation(
+        id = "premium_student",
+        name = "Premium Student",
+        highlights = listOf(
+            "Add-free music listening",
+            "Download to listen offline",
+        ),
+        termsAndConditions = "Offer available only to students at an accredited higher education institution. Terms and conditions apply.",
+        pricingInformation = PricingInformation(
+            associatedCardId = "premium_family",
+            cost = "Free",
+            term = "For 1 month"
+        ),
+        darkColor = Color(0xFFB27049),
+        lightColor = Color(0xFFF49A24)
+    )
+)
