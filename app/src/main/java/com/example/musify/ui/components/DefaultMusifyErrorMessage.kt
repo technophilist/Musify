68b63d0e63c56d56d4d00e6c1b34b29be9b70766
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-10-20 17:33:58 +0530

    refactor: add overload of DefaultMusifyErrorMessage that contains a retry button

--- app/src/main/java/com/example/musify/ui/components/DefaultMusifyErrorMessage.kt
+++ app/src/main/java/com/example/musify/ui/components/DefaultMusifyErrorMessage.kt
@@ -2,16 +2,17 @@ package com.example.musify.ui.components
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material.ContentAlpha
-import androidx.compose.material.LocalTextStyle
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 
 /**
  * A composable that is used to display an error message through-out
@@ -23,7 +24,7 @@ import androidx.compose.ui.text.style.TextAlign
 fun DefaultMusifyErrorMessage(
     title: String,
     subtitle: String,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
 ) {
     Column(
         modifier = modifier,
@@ -42,6 +43,56 @@ fun DefaultMusifyErrorMessage(
                 color = Color.White.copy(alpha = ContentAlpha.disabled)
             ),
             textAlign = TextAlign.Center
+        )
+        Spacer(modifier = Modifier.size(8.dp))
+        OutlinedButton(
+            onClick = { /*TODO*/ },
+            shape = RoundedCornerShape(50),
+            content = { Text(text = "Retry") }
+        )
+    }
+}
+
+/**
+ * A composable that is used to display an error message through-out
+ * the app with the specified [title] and [subtitle]. It manages the
+ * styling of the both the [title] and [subtitle]. This composable
+ * is an overload of [DefaultMusifyErrorMessage] that also displays
+ * a retry button.
+ * @param modifier the modifier to be applied to the composable.
+ * @param onRetryButtonClicked the lambda to execute when the retry
+ * button is clicked.
+ */
+@Composable
+fun DefaultMusifyErrorMessage(
+    title: String,
+    subtitle: String,
+    onRetryButtonClicked: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = title,
+            fontWeight = FontWeight.Bold,
+            style = MaterialTheme.typography.h6,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = subtitle,
+            style = LocalTextStyle.current.copy(
+                color = Color.White.copy(alpha = ContentAlpha.disabled)
+            ),
+            textAlign = TextAlign.Center
+        )
+        Spacer(modifier = Modifier.size(8.dp))
+        OutlinedButton(
+            onClick = onRetryButtonClicked,
+            shape = RoundedCornerShape(50),
+            content = { Text(text = "Retry") }
         )
     }
 }
