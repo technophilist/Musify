9f5694a9bc6a085201b6d1daedee3116cf37f93c
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-10-20 16:57:26 +0530

    refactor: make use of "ExpandableMiniPlayerWithSnackbar" in MusifyApp composable

--- app/src/main/java/com/example/musify/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/musify/ui/activities/MainActivity.kt
@@ -6,26 +6,25 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.compose.animation.*
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.SnackbarHostState
+import androidx.compose.material.Surface
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.musify.domain.SearchResult
 import com.example.musify.ui.components.MusifyBottomNavigation
-import com.example.musify.ui.components.MusifyMiniPlayer
 import com.example.musify.ui.navigation.MusifyBottomNavigationDestinations
 import com.example.musify.ui.screens.MusifyNavigation
-import com.example.musify.ui.screens.NowPlayingScreen
+import com.example.musify.ui.screens.homescreen.ExpandableMiniPlayerWithSnackbar
 import com.example.musify.ui.theme.MusifyTheme
 import com.example.musify.viewmodels.PlaybackViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -63,7 +62,7 @@ private fun MusifyApp() {
     val playbackEvent: PlaybackViewModel.Event? by playbackViewModel.playbackEventsFlow.collectAsState(
         initial = null
     )
-    val miniPlayerTrack = remember(playbackState) {
+    val playerTrack = remember(playbackState) {
         playbackState.currentlyPlayingTrack ?: playbackState.previouslyPlayingTrack
     }
     var isNowPlayingScreenVisible by remember { mutableStateOf(false) }
@@ -85,7 +84,6 @@ private fun MusifyApp() {
             playbackViewModel.playTrack(track)
         }
     }
-    val totalDurationOfCurrentTrackText by playbackViewModel.totalDurationOfCurrentTrackTimeText
     BackHandler(isNowPlayingScreenVisible) {
         isNowPlayingScreenVisible = false
     }
@@ -112,52 +110,22 @@ private fun MusifyApp() {
                 .fillMaxWidth()
                 .align(Alignment.BottomCenter)
         ) {
-            SnackbarHost(
-                modifier = Modifier.padding(8.dp),
-                hostState = snackbarHostState
-            )
             androidx.compose.animation.AnimatedVisibility(
                 visible = playbackState.currentlyPlayingTrack != null || playbackState.previouslyPlayingTrack != null,
                 enter = fadeIn() + slideInVertically { it },
                 exit = fadeOut() + slideOutVertically { -it },
             ) {
-                AnimatedContent(modifier = Modifier.fillMaxWidth(),
-                    targetState = isNowPlayingScreenVisible,
-                    transitionSpec = {
-                        slideInVertically(animationSpec = tween()) with shrinkOut(animationSpec = tween())
-                    }
-                ) { isFullScreenVisible ->
-                    miniPlayerTrack?.let {
-                        if (isFullScreenVisible) {
-                            NowPlayingScreen(
-                                currentlyPlayingTrack = it,
-                                isPlaybackPaused = isPlaybackPaused,
-                                timeElapsedStringFlow = playbackViewModel.flowOfProgressTextOfCurrentTrack.value,
-                                totalDurationOfCurrentTrackProvider = { totalDurationOfCurrentTrackText },
-                                playbackDurationRange = PlaybackViewModel.PLAYBACK_PROGRESS_RANGE,
-                                playbackProgressFlow = playbackViewModel.flowOfProgressOfCurrentTrack.value,
-                                onCloseButtonClicked = { isNowPlayingScreenVisible = false },
-                                onShuffleButtonClicked = { /*TODO*/ },
-                                onSkipPreviousButtonClicked = { /*TODO*/ },
-                                onPlayButtonClicked = { onPlayButtonClicked(miniPlayerTrack) },
-                                onPauseButtonClicked = playbackViewModel::pauseCurrentlyPlayingTrack,
-                                onSkipNextButtonClicked = { /*TODO*/ },
-                                onRepeatButtonClicked = {}
-                            )
-                        } else {
-                            MusifyMiniPlayer(
-                                modifier = Modifier
-                                    .padding(horizontal = 8.dp)
-                                    .clickable { isNowPlayingScreenVisible = true },
-                                isPlaybackPaused = isPlaybackPaused,
-                                currentlyPlayingTrack = it,
-                                onLikedButtonClicked = {},
-                                onPlayButtonClicked = { onPlayButtonClicked(miniPlayerTrack) },
-                                onPauseButtonClicked = playbackViewModel::pauseCurrentlyPlayingTrack
-                            )
-                        }
-
-                    }
+                playerTrack?.let {
+                    ExpandableMiniPlayerWithSnackbar(
+                        track = it,
+                        onPauseButtonClicked = playbackViewModel::pauseCurrentlyPlayingTrack,
+                        onPlayButtonClicked = { trackToPlay -> onPlayButtonClicked(trackToPlay) },
+                        isPlaybackPaused = isPlaybackPaused,
+                        timeElapsedStringFlow = playbackViewModel.flowOfProgressTextOfCurrentTrack.value,
+                        playbackProgressFlow = playbackViewModel.flowOfProgressOfCurrentTrack.value,
+                        totalDurationOfCurrentTrackText = playbackViewModel.totalDurationOfCurrentTrackTimeText.value,
+                        snackbarHostState = snackbarHostState
+                    )
                 }
             }
             MusifyBottomNavigationConnectedWithBackStack(
