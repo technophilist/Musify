7ee837bd0cd6c0e195b1b7d112f881f233ae95d6
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-11-29 15:43:59 +0530

    refactor: add SpotifyPodcastSearchPagingSource

--- app/src/main/java/com/example/musify/data/paging/SpotifySearchPagingSources.kt
+++ app/src/main/java/com/example/musify/data/paging/SpotifySearchPagingSources.kt
@@ -1,8 +1,8 @@
 package com.example.musify.data.paging
 
-import com.example.musify.data.remote.response.toSearchResults
 import com.example.musify.data.remote.musicservice.SearchQueryType
 import com.example.musify.data.remote.musicservice.SpotifyService
+import com.example.musify.data.remote.response.toSearchResults
 import com.example.musify.data.repositories.tokenrepository.TokenRepository
 import com.example.musify.data.utils.MapperImageSize
 import com.example.musify.domain.SearchResult
@@ -69,4 +69,20 @@ fun SpotifyPlaylistSearchPagingSource(
     tokenRepository = tokenRepository,
     spotifyService = spotifyService,
     resultsBlock = { it.toSearchResults(imageSize).playlists }
+)
+
+@Suppress("FunctionName")
+fun SpotifyPodcastSearchPagingSource(
+    searchQuery: String,
+    countryCode: String,
+    imageSize: MapperImageSize,
+    tokenRepository: TokenRepository,
+    spotifyService: SpotifyService
+): SpotifySearchPagingSource<SearchResult.PodcastSearchResult> = SpotifySearchPagingSource(
+    searchQuery = searchQuery,
+    countryCode = countryCode,
+    searchQueryType = SearchQueryType.SHOW,
+    tokenRepository = tokenRepository,
+    spotifyService = spotifyService,
+    resultsBlock = { it.toSearchResults(imageSize).shows }
 )
