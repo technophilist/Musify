12bc25cc114c052e4ae789ecedd573c24e0dd261
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-12-05 19:40:15 +0530

    refactor(EpisodeListCard): add image loading animation

--- app/src/main/java/com/example/musify/ui/components/EpisodeListCard.kt
+++ app/src/main/java/com/example/musify/ui/components/EpisodeListCard.kt
@@ -3,8 +3,7 @@ package com.example.musify.ui.components
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -13,7 +12,6 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
 import com.example.musify.domain.SearchResult
 
 @Suppress("RemoveSingleExpressionStringTemplate")
@@ -36,6 +34,7 @@ fun EpisodeListCard(
     val dateAndDurationString = remember(episodeSearchResult) {
         episodeSearchResult.getDateAndDurationString()
     }
+    var isLoadingPlaceholderVisible by remember { mutableStateOf(true) }
     Card(
         modifier = Modifier
             .height(IntrinsicSize.Min)
@@ -51,13 +50,18 @@ fun EpisodeListCard(
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-            AsyncImage(
+            AsyncImageWithPlaceholder(
                 modifier = Modifier
                     .size(98.dp)
                     .clip(RoundedCornerShape(8.dp)),
                 model = episodeSearchResult.episodeContentInfo.imageUrlString,
                 contentScale = ContentScale.Crop,
                 contentDescription = null,
+                onImageLoading = {
+                    if (!isLoadingPlaceholderVisible) isLoadingPlaceholderVisible = true
+                },
+                onImageLoadingFinished = { isLoadingPlaceholderVisible = false },
+                isLoadingPlaceholderVisible = isLoadingPlaceholderVisible
             )
             Column(
                 modifier = Modifier
