215cd1beeb63a9ea372c433b854d39516f88d7a9
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-12-12 12:46:44 +0530

    refactor(SpotifyService): add getEpisodeWithId()

--- app/src/main/java/com/example/musify/data/remote/musicservice/SpotifyService.kt
+++ app/src/main/java/com/example/musify/data/remote/musicservice/SpotifyService.kt
@@ -166,8 +166,15 @@ interface SpotifyService {
     suspend fun getPlaylistsForCategory(
         @Header("Authorization") token: BearerToken,
         @Path("category_id") categoryId: String,
-        @Query("country") market: String,
+        @Query("country") market: String, // TODO change query name to "market"
         @Query("limit") limit: Int = 20,
         @Query("offset") offset: Int = 0
     ): PlaylistsForSpecificCategoryResponse
+
+    @GET(SpotifyEndPoints.SPECIFIC_EPISODE_ENDPOINT)
+    suspend fun getEpisodeWithId(
+        @Header("Authorization") token: BearerToken,
+        @Path("id") id:String,
+        @Query("market") market:String
+    ):EpisodeResponse
 }
