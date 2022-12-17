2a791ed3a8cc14373a2c04629af81dadeef73c6e
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-12-17 17:31:38 +0530

    refactor: add PodcastDetailViewModel

---
+++ app/src/main/java/com/example/musify/viewmodels/PodcastDetailViewModel.kt
@@ -0,0 +1,63 @@
+package com.example.musify.viewmodels
+
+import android.app.Application
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.viewModelScope
+import com.example.musify.data.repositories.podcastsrepository.PodcastsRepository
+import com.example.musify.data.utils.FetchedResource
+import com.example.musify.data.utils.MapperImageSize
+import com.example.musify.domain.PodcastEpisode
+import com.example.musify.ui.navigation.MusifyNavigationDestinations
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class PodcastDetailViewModel @Inject constructor(
+    application: Application,
+    private val podcastsRepository: PodcastsRepository,
+    private val savedStateHandle: SavedStateHandle,
+) : AndroidViewModel(application) {
+
+    enum class UiSate { IDLE, LOADING, ERROR }
+
+    private val _uiState = mutableStateOf(UiSate.IDLE)
+    val uiState = _uiState as State<UiSate>
+    private val _podcastEpisode = mutableStateOf<PodcastEpisode?>(null)
+    val podcastEpisode = _podcastEpisode as State<PodcastEpisode?>
+
+    init {
+        fetchEpisodeUpdatingUiState()
+    }
+
+    private fun fetchEpisodeUpdatingUiState() {
+        viewModelScope.launch {
+            _uiState.value = UiSate.LOADING
+            val episode = fetchEpisode()
+            _uiState.value = if (episode == null) {
+                UiSate.ERROR
+            } else {
+                _podcastEpisode.value = episode
+                UiSate.IDLE
+            }
+        }
+    }
+
+    private suspend fun fetchEpisode(): PodcastEpisode? {
+        val fetchedResource = podcastsRepository.fetchPodcastEpisode(
+            episodeId = savedStateHandle[MusifyNavigationDestinations.PodcastEpisodeDetailScreen.NAV_ARG_PODCAST_EPISODE_ID]!!,
+            countryCode = getCountryCode(),
+            imageSize = MapperImageSize.SMALL
+        )
+        return if (fetchedResource is FetchedResource.Success) fetchedResource.data else null
+    }
+
+    fun retryFetchingEpisode() {
+        fetchEpisodeUpdatingUiState()
+    }
+
+
+}
