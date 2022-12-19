682022454a9932df1b32f2803d3e3ef9d45024f1
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-12-19 19:37:17 +0530

    refactor: add Streamable#toMusicPlayerTrack extension

--- app/src/main/java/com/example/musify/domain/Streamable.kt
+++ app/src/main/java/com/example/musify/domain/Streamable.kt
@@ -1,5 +1,8 @@
 package com.example.musify.domain
 
+import android.graphics.Bitmap
+import com.example.musify.musicplayer.MusicPlayerV2
+
 /**
  * Indicates a class that contains a streamable url. It is possible
  * that a class may contain a null [streamUrl] because the
@@ -9,4 +12,12 @@ package com.example.musify.domain
  */
 sealed interface Streamable {
     val streamUrl: String?
+}
+
+/**
+ * A mapper method used to map an instance of [Streamable] to an instance of [MusicPlayerV2.Track].
+ */
+fun Streamable.toMusicPlayerTrack(albumArtBitmap: Bitmap): MusicPlayerV2.Track = when (this) {
+    is PodcastEpisode -> toMusicPlayerTrack(albumArtBitmap) // PodcastEpisode#toMusicPlayerTrack
+    is SearchResult.TrackSearchResult -> toMusicPlayerTrack(albumArtBitmap) // TrackSearchResult#toMusicPlayerTrack
 }
