c247d4ce2da5e45b84d2c84d6979d7deef4d836e
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-12-19 19:29:07 +0530

    refactor: add PodcastEpisode#toMusicPlayerTrack mapper

--- app/src/main/java/com/example/musify/domain/PodcastEpisode.kt
+++ app/src/main/java/com/example/musify/domain/PodcastEpisode.kt
@@ -1,6 +1,8 @@
 package com.example.musify.domain
 
+import android.graphics.Bitmap
 import android.text.Spanned
+import com.example.musify.musicplayer.MusicPlayerV2
 
 /**
  * A domain class that represents a specific podcast episode.
@@ -36,4 +38,20 @@ data class PodcastEpisode(
      * A domain class that contains the duration information of a [PodcastEpisode].
      */
     data class DurationInfo(val hours: Int, val minutes: Int)
+}
+
+/**
+ * A mapper method used to map an instance of
+ * [SearchResult.PodcastSearchResult] to an instance of [MusicPlayerV2.Track].
+ */
+fun PodcastEpisode.toMusicPlayerTrack(imageBitmap: Bitmap): MusicPlayerV2.Track {
+    if (previewUrl == null) throw IllegalStateException("The previewUrl cannot be null during conversion")
+    return MusicPlayerV2.Track(
+        id = id,
+        title = title,
+        artistsString = this.podcastInfo.name,
+        albumArt = imageBitmap,
+        albumArtUrlString = this.podcastInfo.imageUrl,
+        trackUrlString = this.previewUrl
+    )
 }
