6e25d3c850c6e38c0d52633c63bd378a20d3a20c
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-12-20 17:17:43 +0530

    docs: add class docs for MusicPlayerV2.Track

--- app/src/main/java/com/example/musify/musicplayer/MusicPlayerV2.kt
+++ app/src/main/java/com/example/musify/musicplayer/MusicPlayerV2.kt
@@ -1,6 +1,8 @@
 package com.example.musify.musicplayer
 
 import android.graphics.Bitmap
+import com.example.musify.domain.SearchResult
+import com.example.musify.musicplayer.utils.toTrackSearchResult
 import kotlinx.coroutines.flow.Flow
 
 interface MusicPlayerV2 {
@@ -17,6 +19,17 @@ interface MusicPlayerV2 {
         object Idle : PlaybackState()
     }
 
+    /**
+     * A class that represents a [Track] playable by a concrete
+     * implementation of [MusicPlayerV2].
+     * Note: This class has both [albumArt] and [albumArtUrlString] as
+     * properties. It might seem redundant to have both [albumArtUrlString]
+     * since the the [albumArt]. The main reason as to why [albumArtUrlString]
+     * exists is because this information can be used when converting an instance
+     * of [Track] to some other domain object. For example, [albumArtUrlString]
+     * will be used when converting an instance of [Track] to an instance of
+     * [SearchResult.TrackSearchResult] using the [toTrackSearchResult].
+     */
     data class Track(
         val id: String,
         val title: String,
