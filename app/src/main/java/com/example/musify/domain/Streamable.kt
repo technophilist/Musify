247a60404c891d3c781fe72a4ab11cc655c2b1b4
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-12-24 12:47:26 +0530

    fix: remove Streamable#toMusicPlayerTrack

--- app/src/main/java/com/example/musify/domain/Streamable.kt
+++ app/src/main/java/com/example/musify/domain/Streamable.kt
@@ -1,8 +1,5 @@
 package com.example.musify.domain
 
-import android.graphics.Bitmap
-import com.example.musify.musicplayer.MusicPlayerV2
-
 /**
  * Indicates a class that contains a streamable url. It is possible
  * that a class may contain a null [streamUrl] because the
@@ -12,12 +9,4 @@ import com.example.musify.musicplayer.MusicPlayerV2
  */
 sealed interface Streamable {
     val streamUrl: String?
-}
-
-/**
- * A mapper method used to map an instance of [Streamable] to an instance of [MusicPlayerV2.Track].
- */
-fun Streamable.toMusicPlayerTrack(albumArtBitmap: Bitmap): MusicPlayerV2.Track = when (this) {
-    is PodcastEpisode -> toMusicPlayerTrack(albumArtBitmap) // PodcastEpisode#toMusicPlayerTrack
-    is SearchResult.TrackSearchResult -> toMusicPlayerTrack(albumArtBitmap) // TrackSearchResult#toMusicPlayerTrack
 }
