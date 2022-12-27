93ff62d4d8eb99da2906e785be9fdc14a9192d29
‘t3chkid’ <54663474+t3chkid@users.noreply.github.com>
2022-12-27 16:34:26 +0530

    fix(AlbumDetailViewModel): add missing call to launchIn() for loadingStatusStream

--- app/src/main/java/com/example/musify/viewmodels/AlbumDetailViewModel.kt
+++ app/src/main/java/com/example/musify/viewmodels/AlbumDetailViewModel.kt
@@ -14,6 +14,7 @@ import com.example.musify.ui.navigation.MusifyNavigationDestinations
 import com.example.musify.usecases.getCurrentlyPlayingTrackUseCase.GetCurrentlyPlayingTrackUseCase
 import com.example.musify.usecases.getPlaybackLoadingStatusUseCase.GetPlaybackLoadingStatusUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -59,7 +60,7 @@ class AlbumDetailViewModel @Inject constructor(
                     _uiState.value = AlbumDetailUiState.Idle
                     return@onEach
                 }
-            }
+            }.launchIn(viewModelScope)
     }
 
     private fun fetchAndAssignTrackList() {
