433b0625abae3b8e47f0cb667077c0d9cf7e4425
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-01-08 16:04:09 +0530

    refactor: add/impl GetCurrentlyPlayingStreamableUseCase

--- app/src/main/java/com/example/musify/usecases/getPlaybackLoadingStatusUseCase/GetPlaybackLoadingStatusUseCase.kt
+++ app/src/main/java/com/example/musify/usecases/getPlaybackLoadingStatusUseCase/GetPlaybackLoadingStatusUseCase.kt
@@ -2,6 +2,7 @@ package com.example.musify.usecases.getPlaybackLoadingStatusUseCase
 
 import kotlinx.coroutines.flow.Flow
 
+@Deprecated("Use GetCurrentlyPlayingStreamable")
 interface GetPlaybackLoadingStatusUseCase {
     val loadingStatusStream: Flow<Boolean>
 }
