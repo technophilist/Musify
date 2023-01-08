cedc27670da28654ffe9aefec4b1e509056bda10
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-01-08 16:04:48 +0530

    refactor: deprecate GetCurrentlyPlayingTrack/PodcastEpisodeUseCase

--- app/src/main/java/com/example/musify/usecases/getCurrentlyPlayingPodcastEpisodeUseCase/GetCurrentlyPlayingTrackUseCase.kt
+++ app/src/main/java/com/example/musify/usecases/getCurrentlyPlayingPodcastEpisodeUseCase/GetCurrentlyPlayingTrackUseCase.kt
@@ -3,6 +3,7 @@ package com.example.musify.usecases.getCurrentlyPlayingPodcastEpisodeUseCase
 import com.example.musify.domain.PodcastEpisode
 import kotlinx.coroutines.flow.Flow
 
+@Deprecated("Use GetCurrentlyPlayingStreamable")
 interface GetCurrentlyPlayingPodcastEpisodeUseCase {
     fun getCurrentlyPlayingPodcastEpisodeStream(): Flow<PodcastEpisode?>
 }
--- app/src/main/java/com/example/musify/usecases/getCurrentlyPlayingTrackUseCase/GetCurrentlyPlayingTrackUseCase.kt
+++ app/src/main/java/com/example/musify/usecases/getCurrentlyPlayingTrackUseCase/GetCurrentlyPlayingTrackUseCase.kt
@@ -3,6 +3,7 @@ package com.example.musify.usecases.getCurrentlyPlayingTrackUseCase
 import com.example.musify.domain.SearchResult
 import kotlinx.coroutines.flow.Flow
 
+@Deprecated("Use GetCurrentlyPlayingStreamable")
 interface GetCurrentlyPlayingTrackUseCase {
     fun getCurrentlyPlayingTrackStream(): Flow<SearchResult.TrackSearchResult?>
 }
