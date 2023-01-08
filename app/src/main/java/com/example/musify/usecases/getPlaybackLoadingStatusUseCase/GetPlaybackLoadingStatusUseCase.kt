884d296fe0b135dbaf54011a0949df4d434b174b
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-01-08 20:49:00 +0530

    fix: remove incorrect deprecation of GetPlaybackLoadingStatusUseCase

--- app/src/main/java/com/example/musify/usecases/getPlaybackLoadingStatusUseCase/GetPlaybackLoadingStatusUseCase.kt
+++ app/src/main/java/com/example/musify/usecases/getPlaybackLoadingStatusUseCase/GetPlaybackLoadingStatusUseCase.kt
@@ -2,7 +2,6 @@ package com.example.musify.usecases.getPlaybackLoadingStatusUseCase
 
 import kotlinx.coroutines.flow.Flow
 
-@Deprecated("Use GetCurrentlyPlayingStreamable")
 interface GetPlaybackLoadingStatusUseCase {
     val loadingStatusStream: Flow<Boolean>
 }
