54150a8099e042e977e9f33ce0fc37703c4d948f
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-01-09 19:44:19 +0530

    refactor(PodcastShowDetailViewModel): add isCurrentlyPlayingEpisodePaused state

--- app/src/main/java/com/example/musify/viewmodels/PodcastShowDetailViewModel.kt
+++ app/src/main/java/com/example/musify/viewmodels/PodcastShowDetailViewModel.kt
@@ -11,6 +11,7 @@ import com.example.musify.data.utils.FetchedResource
 import com.example.musify.data.utils.MapperImageSize
 import com.example.musify.domain.PodcastShow
 import com.example.musify.domain.SearchResult
+import com.example.musify.musicplayer.MusicPlayerV2
 import com.example.musify.ui.navigation.MusifyNavigationDestinations
 import com.example.musify.usecases.getCurrentlyPlayingStreamableUseCase.GetCurrentlyPlayingStreamableUseCase
 import com.example.musify.usecases.getPlaybackLoadingStatusUseCase.GetPlaybackLoadingStatusUseCase
@@ -25,6 +26,7 @@ import javax.inject.Inject
 class PodcastShowDetailViewModel @Inject constructor(
     application: Application,
     savedStateHandle: SavedStateHandle,
+    musicPlayer: MusicPlayerV2,
     getCurrentlyPlayingStreamableUseCase: GetCurrentlyPlayingStreamableUseCase,
     getPlaybackLoadingStatusUseCase: GetPlaybackLoadingStatusUseCase,
     private val podcastsRepository: PodcastsRepository
@@ -45,9 +47,14 @@ class PodcastShowDetailViewModel @Inject constructor(
     private val _podcastShow = mutableStateOf<PodcastShow?>(null)
     val podcastShow = _podcastShow as State<PodcastShow?>
 
+    // TODO STOPSHIP when playback is done, the pause button is still
+    // displayed in the ui
     val currentlyPlayingEpisode = getCurrentlyPlayingStreamableUseCase
         .currentlyPlayingStreamableFlow
         .filterIsInstance<SearchResult.StreamableEpisodeSearchResult>()
+
+    private val _isCurrentlyPlayingEpisodePaused = mutableStateOf<Boolean?>(null)
+    val isCurrentlyPlayingEpisodePaused = _isCurrentlyPlayingEpisodePaused as State<Boolean?>
 
     init {
         fetchShowUpdatingUiState()
@@ -59,6 +66,17 @@ class PodcastShowDetailViewModel @Inject constructor(
                     return@onEach
                 }
                 if (_uiState.value == UiState.PLAYBACK_LOADING) _uiState.value = UiState.IDLE
+            }.launchIn(viewModelScope)
+        musicPlayer
+            .currentPlaybackStateStream
+            .onEach {
+                if (it is MusicPlayerV2.PlaybackState.Playing && (_isCurrentlyPlayingEpisodePaused.value == null || _isCurrentlyPlayingEpisodePaused.value == true)) {
+                    _isCurrentlyPlayingEpisodePaused.value = false
+                    return@onEach
+                }
+                if (it is MusicPlayerV2.PlaybackState.Paused && _isCurrentlyPlayingEpisodePaused.value == false) {
+                    _isCurrentlyPlayingEpisodePaused.value = true
+                }
             }.launchIn(viewModelScope)
     }
 
