20cb56f3297d52874198872b5bc3546c1e311bfb
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-01-11 17:54:52 +0530

    refactor: un-deprecate "GetCurrentlyPlayingTrackUseCase"

--- app/src/main/java/com/example/musify/usecases/getCurrentlyPlayingTrackUseCase/GetCurrentlyPlayingTrackUseCase.kt
+++ app/src/main/java/com/example/musify/usecases/getCurrentlyPlayingTrackUseCase/GetCurrentlyPlayingTrackUseCase.kt
@@ -3,7 +3,6 @@ package com.example.musify.usecases.getCurrentlyPlayingTrackUseCase
 import com.example.musify.domain.SearchResult
 import kotlinx.coroutines.flow.Flow
 
-@Deprecated("Use GetCurrentlyPlayingStreamable")
 interface GetCurrentlyPlayingTrackUseCase {
     fun getCurrentlyPlayingTrackStream(): Flow<SearchResult.TrackSearchResult?>
 }
