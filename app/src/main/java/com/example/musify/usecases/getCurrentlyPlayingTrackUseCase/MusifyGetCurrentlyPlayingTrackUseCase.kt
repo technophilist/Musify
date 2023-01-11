0567866f031e9d3a5f734443456d945d24fc6171
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-01-11 18:00:04 +0530

    refactor(MusifyGetCurrentlyPlayingTrackUseCase): replace use of MusicPlayer with GetCurrentlyPlayingStreamableUseCase

--- app/src/main/java/com/example/musify/usecases/getCurrentlyPlayingTrackUseCase/MusifyGetCurrentlyPlayingTrackUseCase.kt
+++ app/src/main/java/com/example/musify/usecases/getCurrentlyPlayingTrackUseCase/MusifyGetCurrentlyPlayingTrackUseCase.kt
@@ -1,19 +1,17 @@
 package com.example.musify.usecases.getCurrentlyPlayingTrackUseCase
 
 import com.example.musify.domain.SearchResult
-import com.example.musify.musicplayer.MusicPlayerV2
+import com.example.musify.usecases.getCurrentlyPlayingStreamableUseCase.GetCurrentlyPlayingStreamableUseCase
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.filterIsInstance
-import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class MusifyGetCurrentlyPlayingTrackUseCase @Inject constructor(
-    val musicPlayer: MusicPlayerV2
+    private val getCurrentlyPlayingStreamableUseCase: GetCurrentlyPlayingStreamableUseCase
 ) : GetCurrentlyPlayingTrackUseCase {
+    @Suppress("RemoveExplicitTypeArguments")
     override fun getCurrentlyPlayingTrackStream(): Flow<SearchResult.TrackSearchResult> =
-        musicPlayer.currentPlaybackStateStream
-            .filterIsInstance<MusicPlayerV2.PlaybackState.Playing>()
-            .filter { it.currentlyPlayingStreamable is SearchResult.TrackSearchResult }
-            .map { it.currentlyPlayingStreamable as SearchResult.TrackSearchResult }
+        getCurrentlyPlayingStreamableUseCase
+            .currentlyPlayingStreamableFlow
+            .filterIsInstance<SearchResult.TrackSearchResult>()
 }
