ffd31d4e08414e09a7c045d698ec67287ffea129
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-01-12 12:54:01 +0530

    refactor: remove GetCurrentlyPlayingPodcastEpisodeUseCase interface & impl

--- app/src/main/java/com/example/musify/usecases/getCurrentlyPlayingPodcastEpisodeUseCase/GetCurrentlyPlayingPodcastEpisodeUseCase.kt
+++
@@ -1,9 +0,0 @@
-package com.example.musify.usecases.getCurrentlyPlayingPodcastEpisodeUseCase
-
-import com.example.musify.domain.PodcastEpisode
-import kotlinx.coroutines.flow.Flow
-
-@Deprecated("Use GetCurrentlyPlayingStreamable")
-interface GetCurrentlyPlayingPodcastEpisodeUseCase {
-    fun getCurrentlyPlayingPodcastEpisodeStream(): Flow<PodcastEpisode?>
-}
