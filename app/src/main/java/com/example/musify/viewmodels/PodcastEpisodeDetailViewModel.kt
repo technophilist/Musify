4ba3cfaaaca01b214ecb15943eac5a86412a2548
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-01-14 18:05:59 +0530

    refactor(PodcastEpisodeDetailViewModel): inject and use GetCurrentlyPlayingEpisodePlaybackStateUseCase

--- app/src/main/java/com/example/musify/viewmodels/PodcastEpisodeDetailViewModel.kt
+++ app/src/main/java/com/example/musify/viewmodels/PodcastEpisodeDetailViewModel.kt
@@ -1,7 +1,8 @@
 package com.example.musify.viewmodels
 
 import android.app.Application
-import androidx.compose.runtime.*
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.viewModelScope
@@ -9,25 +10,21 @@ import com.example.musify.data.repositories.podcastsrepository.PodcastsRepositor
 import com.example.musify.data.utils.FetchedResource
 import com.example.musify.data.utils.MapperImageSize
 import com.example.musify.domain.PodcastEpisode
-import com.example.musify.musicplayer.MusicPlayerV2
 import com.example.musify.ui.navigation.MusifyNavigationDestinations
-import com.example.musify.usecases.getCurrentlyPlayingStreamableUseCase.GetCurrentlyPlayingStreamableUseCase
-import com.example.musify.usecases.getPlaybackLoadingStatusUseCase.GetPlaybackLoadingStatusUseCase
+import com.example.musify.usecases.getCurrentlyPlayingEpisodePlaybackStateUseCase.GetCurrentlyPlayingEpisodePlaybackStateUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.filterIsInstance
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import javax.inject.Inject
+import com.example.musify.usecases.getCurrentlyPlayingEpisodePlaybackStateUseCase.GetCurrentlyPlayingEpisodePlaybackStateUseCase.PlaybackState as UseCasePlaybackState
 
 @HiltViewModel
 class PodcastEpisodeDetailViewModel @Inject constructor(
     application: Application,
     private val podcastsRepository: PodcastsRepository,
     private val savedStateHandle: SavedStateHandle,
-    musicPlayerV2: MusicPlayerV2,
-    getCurrentlyPlayingStreamableUseCase: GetCurrentlyPlayingStreamableUseCase,
-    getPlaybackLoadingStatusUseCase: GetPlaybackLoadingStatusUseCase
+    getEpisodePlaybackStateUseCase: GetCurrentlyPlayingEpisodePlaybackStateUseCase,
 ) : AndroidViewModel(application) {
 
     enum class UiSate { IDLE, LOADING, PLAYBACK_LOADING, ERROR }
@@ -37,46 +34,25 @@ class PodcastEpisodeDetailViewModel @Inject constructor(
     private val _podcastEpisode = mutableStateOf<PodcastEpisode?>(null)
     val podcastEpisode = _podcastEpisode as State<PodcastEpisode?>
 
-    private var isMusicPlayerPlaying by mutableStateOf<Boolean?>(null)
-    private var currentlyPlayingPodcastEpisode by mutableStateOf<PodcastEpisode?>(null)
 
-    val isEpisodeCurrentlyPlaying = derivedStateOf {
-        isMusicPlayerPlaying == true && currentlyPlayingPodcastEpisode == podcastEpisode.value
-    }
+    private var _isEpisodeCurrentlyPlaying = mutableStateOf(false)
+    val isEpisodeCurrentlyPlaying = _isEpisodeCurrentlyPlaying as State<Boolean>
 
     init {
         fetchEpisodeUpdatingUiState()
-        
-        getPlaybackLoadingStatusUseCase
-            .loadingStatusStream
-            .onEach { isPlaybackLoading ->
-                if (isPlaybackLoading && _uiState.value != UiSate.PLAYBACK_LOADING) {
-                    _uiState.value = UiSate.PLAYBACK_LOADING
-                    return@onEach
-                }
-                if (!isPlaybackLoading && _uiState.value == UiSate.PLAYBACK_LOADING) {
-                    _uiState.value = UiSate.IDLE
-                    return@onEach
-                }
-            }
-            .launchIn(viewModelScope)
-
-        getCurrentlyPlayingStreamableUseCase
-            .currentlyPlayingStreamableStream
-            .filterIsInstance<PodcastEpisode>()
-            .onEach { currentlyPlayingPodcastEpisode = it }
-            .launchIn(viewModelScope)
-        musicPlayerV2.currentPlaybackStateStream
+        getEpisodePlaybackStateUseCase
+            .currentlyPlayingEpisodePlaybackStateStream
             .onEach {
-                if (it is MusicPlayerV2.PlaybackState.Playing && (isMusicPlayerPlaying == false || isMusicPlayerPlaying == null)) {
-                    isMusicPlayerPlaying = true
-                    return@onEach
+                when (it) {
+                    is UseCasePlaybackState.Ended -> _isEpisodeCurrentlyPlaying.value = false
+                    is UseCasePlaybackState.Loading -> _uiState.value = UiSate.PLAYBACK_LOADING
+                    is UseCasePlaybackState.Paused -> _isEpisodeCurrentlyPlaying.value = false
+                    is UseCasePlaybackState.Playing -> {
+                        if (_uiState.value == UiSate.PLAYBACK_LOADING) _uiState.value = UiSate.IDLE
+                        _isEpisodeCurrentlyPlaying.value = true
+                    }
                 }
-                if (it is MusicPlayerV2.PlaybackState.Ended || it is MusicPlayerV2.PlaybackState.Paused) {
-                    if (isMusicPlayerPlaying == true) isMusicPlayerPlaying = false
-                }
-            }
-            .launchIn(viewModelScope)
+            }.launchIn(viewModelScope)
     }
 
     private fun fetchEpisodeUpdatingUiState() {
