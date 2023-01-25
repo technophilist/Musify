b73add3252fe87c507b4e10cfdd6102da776d8cd
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-01-25 14:11:30 +0530

    refactor: remove un-used TrackResponseWithoutAlbumMetadataResponse#toTrackSearchResult overload

--- app/src/main/java/com/example/musify/data/remote/response/AlbumResponse.kt
+++ app/src/main/java/com/example/musify/data/remote/response/AlbumResponse.kt
@@ -87,20 +87,6 @@ fun AlbumResponse.getTracks(): List<SearchResult.TrackSearchResult> =
  * to an instance of [SearchResult.TrackSearchResult].
  */
 fun AlbumResponse.TrackResponseWithoutAlbumMetadataResponse.toTrackSearchResult(
-    albumArtImageUrlString: String,
-    albumArtistsString: String
-) = SearchResult.TrackSearchResult(
-    id = id,
-    name = name,
-    largeImageUrlString = albumArtImageUrlString,
-    artistsString = albumArtistsString,
-    trackUrlString = previewUrl
-)
-
-/**
- * todo - add doc
- */
-fun AlbumResponse.TrackResponseWithoutAlbumMetadataResponse.toTrackSearchResult(
     largeAlbumArtImageUrlString: String,
     smallAlbumArtImageUrlString: String,
     albumArtistsString: String
