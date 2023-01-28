f6a2e4a97a075a6efe5760256df09039fd061995
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-01-28 19:28:41 +0530

    test(MusicPlayerMock): implement currentlyPlayingStreamable property removing TODO()

--- app/src/test/java/com/example/musify/musicPlayer/MusicPlayerMock.kt
+++ app/src/test/java/com/example/musify/musicPlayer/MusicPlayerMock.kt
@@ -2,47 +2,38 @@ package com.example.musify.musicPlayer
 
 import android.graphics.Bitmap
 import com.example.musify.domain.Streamable
+import com.example.musify.domain.fakeTrackSearchResult
 import com.example.musify.musicplayer.MusicPlayerV2
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.emptyFlow
 
 class MusicPlayerMock : MusicPlayerV2 {
-    private lateinit var onPlaybackStateChangedCallback: (MusicPlayerV2.PlaybackState) -> Unit
     private var currentlyPlayingStreamable: Streamable? = null
-    private var isPlaybackPaused = false
+    private val _currentPlaybackStateStream =
+        MutableStateFlow<MusicPlayerV2.PlaybackState>(MusicPlayerV2.PlaybackState.Idle)
+    override val currentPlaybackStateStream =
+        _currentPlaybackStateStream as Flow<MusicPlayerV2.PlaybackState>
+
     override fun pauseCurrentlyPlayingTrack() {
-        val pausedState =
+        _currentPlaybackStateStream.value =
             currentlyPlayingStreamable?.let(MusicPlayerV2.PlaybackState::Paused) ?: return
-        onPlaybackStateChangedCallback(pausedState)
     }
 
     override fun stopPlayingTrack() {
-        onPlaybackStateChangedCallback(MusicPlayerV2.PlaybackState.Idle)
+        _currentPlaybackStateStream.value =
+            currentlyPlayingStreamable?.let(MusicPlayerV2.PlaybackState::Ended) ?: return
     }
 
-    override val currentPlaybackStateStream: Flow<MusicPlayerV2.PlaybackState>
-        get() = TODO("Not yet implemented")
 
     override fun playStreamable(streamable: Streamable, associatedAlbumArt: Bitmap) {
-        if (currentlyPlayingStreamable == streamable) {
-            return
-        }
         currentlyPlayingStreamable = streamable
-        onPlaybackStateChangedCallback(
-            MusicPlayerV2.PlaybackState.Playing(
-                currentlyPlayingStreamable = streamable,
-                totalDuration = 5_000,
-                currentPlaybackPositionInMillisFlow = flow {
-                    for (i in 1..5) {
-                        emit(i * 1_000L)
-                        while (isPlaybackPaused) emit(i * 1000L)
-                        delay(1_000)
-                    }
-                }.distinctUntilChanged()
-            )
+        _currentPlaybackStateStream.value = MusicPlayerV2.PlaybackState.Playing(
+            currentlyPlayingStreamable = fakeTrackSearchResult,
+            totalDuration = 10L,
+            currentPlaybackPositionInMillisFlow = emptyFlow()
         )
+
     }
 
     override fun tryResume(): Boolean = currentlyPlayingStreamable != null
