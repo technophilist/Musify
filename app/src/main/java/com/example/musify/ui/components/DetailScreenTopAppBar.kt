423a6789fe9c901fdc63adb3af06c7811f22028d
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-02-06 13:19:22 +0530

    refactor: remove redundant DetailScreenTopAppBar overload

--- app/src/main/java/com/example/musify/ui/components/DetailScreenTopAppBar.kt
+++ app/src/main/java/com/example/musify/ui/components/DetailScreenTopAppBar.kt
@@ -9,7 +9,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
@@ -22,72 +21,6 @@ import com.example.musify.ui.dynamicbackgroundmodifier.DynamicBackgroundResource
 import com.example.musify.ui.dynamicbackgroundmodifier.DynamicBackgroundStyle
 import com.example.musify.ui.dynamicbackgroundmodifier.dynamicBackground
 import com.example.musify.ui.theme.MusifyTheme
-import com.example.musify.ui.theme.dynamictheme.DynamicBackgroundType
-import com.example.musify.ui.theme.dynamictheme.DynamicThemeResource
-import com.example.musify.ui.theme.dynamictheme.DynamicallyThemedSurface
-
-/**
- * An appbar that is meant to be used in a detail screen. It is mainly
- * used to display the [title] with a back button.
- * @param title the title to be displayed.
- * @param onBackButtonClicked the lambda to execute with the user clicks
- * on the back button.
- * @param modifier the modifier to be applied to the app bar.
- * @param onClick the lambda to execute when the app bar clicked. This is
- * usually used to scroll a list to the first item.
- * @param dynamicThemeResource the resource to be ugit status
- * sed to set the
- * background color. By default, it is set to [DynamicThemeResource.Empty].
- */
-@Composable
-fun DetailScreenTopAppBar(
-    title: String,
-    onBackButtonClicked: () -> Unit,
-    modifier: Modifier = Modifier,
-    onClick: () -> Unit = {},
-    dynamicThemeResource: DynamicThemeResource = DynamicThemeResource.Empty
-) {
-    val dynamicThemeBackgroundType = remember {
-        DynamicBackgroundType.Filled(scrimColor = Color.Black.copy(alpha = 0.3f))
-    }
-    DynamicallyThemedSurface(
-        modifier = Modifier.shadow(elevation = AppBarDefaults.TopAppBarElevation),
-        dynamicThemeResource = dynamicThemeResource,
-        dynamicBackgroundType = dynamicThemeBackgroundType
-    ) {
-        // Since the top app bar's background color is transparent,
-        // any elevation to the app bar would make it look like it has
-        // a border. Therefore, set the elevation to 0dp.
-        TopAppBar(
-            modifier = modifier.clickable(onClick = onClick),
-            backgroundColor = Color.Transparent,
-            elevation = 0.dp
-        ) {
-            IconButton(
-                modifier = Modifier
-                    .clip(CircleShape)
-                    .align(Alignment.CenterVertically)
-                    .offset(y = 1.dp),
-                onClick = onBackButtonClicked
-            ) {
-                Icon(
-                    imageVector = ImageVector.vectorResource(id = R.drawable.ic_baseline_chevron_left_24),
-                    contentDescription = null,
-                    tint = Color.White
-                )
-            }
-            Text(
-                modifier = Modifier.align(Alignment.CenterVertically),
-                text = title,
-                style = MaterialTheme.typography.body1,
-                fontWeight = FontWeight.SemiBold,
-                color = Color.White,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis
-            )
-        }
-    }
-}
 
 /**
  * An appbar that is meant to be used in a detail screen. It is mainly
@@ -158,7 +91,7 @@ fun DetailScreenTopAppBarPreview() {
         DetailScreenTopAppBar(
             title = "Title",
             onBackButtonClicked = {},
-            dynamicThemeResource = DynamicThemeResource.Empty
+            dynamicBackgroundResource = DynamicBackgroundResource.Empty
         )
     }
 }
