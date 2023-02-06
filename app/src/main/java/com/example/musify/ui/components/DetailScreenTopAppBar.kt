3ca47f3425fa8a559930e8df4cc7291af11d40ac
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-02-06 12:54:20 +0530

    refactor: add overload of DetailScreenTopAppBar that uses the dynamicBackground modifier

--- app/src/main/java/com/example/musify/ui/components/DetailScreenTopAppBar.kt
+++ app/src/main/java/com/example/musify/ui/components/DetailScreenTopAppBar.kt
@@ -18,6 +18,9 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.musify.R
+import com.example.musify.ui.dynamicbackgroundmodifier.DynamicBackgroundResource
+import com.example.musify.ui.dynamicbackgroundmodifier.DynamicBackgroundStyle
+import com.example.musify.ui.dynamicbackgroundmodifier.dynamicBackground
 import com.example.musify.ui.theme.MusifyTheme
 import com.example.musify.ui.theme.dynamictheme.DynamicBackgroundType
 import com.example.musify.ui.theme.dynamictheme.DynamicThemeResource
@@ -86,13 +89,76 @@ fun DetailScreenTopAppBar(
     }
 }
 
+/**
+ * An appbar that is meant to be used in a detail screen. It is mainly
+ * used to display the [title] with a back button. This overload
+ * uses the [Modifier.dynamicBackground] modifier.
+ *
+ * @param title the title to be displayed.
+ * @param onBackButtonClicked the lambda to execute with the user clicks
+ * on the back button.
+ * @param modifier the modifier to be applied to the app bar.
+ * @param onClick the lambda to execute when the app bar clicked. This is
+ * usually used to scroll a list to the first item.
+ * @param dynamicBackgroundResource the resource from which the background
+ * color would be extracted from. By default, it is set to [DynamicBackgroundResource.Empty].
+ */
+@Composable
+fun DetailScreenTopAppBar(
+    title: String,
+    onBackButtonClicked: () -> Unit,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = {},
+    dynamicBackgroundResource: DynamicBackgroundResource = DynamicBackgroundResource.Empty
+) {
+    val dynamicBackgroundStyle = remember {
+        DynamicBackgroundStyle.Filled(scrimColor = Color.Black.copy(alpha = 0.3f))
+    }
+
+    // Since the top app bar's background color is transparent,
+    // any elevation to the app bar would make it look like it has
+    // a border. Therefore, set the elevation to 0dp.
+    TopAppBar(
+        modifier = Modifier
+            .dynamicBackground(dynamicBackgroundResource, dynamicBackgroundStyle)
+            .then(modifier)
+            .clickable(onClick = onClick),
+        backgroundColor = Color.Transparent,
+        elevation = 0.dp
+    ) {
+        IconButton(
+            modifier = Modifier
+                .clip(CircleShape)
+                .align(Alignment.CenterVertically)
+                .offset(y = 1.dp),
+            onClick = onBackButtonClicked
+        ) {
+            Icon(
+                imageVector = ImageVector.vectorResource(id = R.drawable.ic_baseline_chevron_left_24),
+                contentDescription = null,
+                tint = Color.White
+            )
+        }
+        Text(
+            modifier = Modifier.align(Alignment.CenterVertically),
+            text = title,
+            style = MaterialTheme.typography.body1,
+            fontWeight = FontWeight.SemiBold,
+            color = Color.White,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+    }
+}
+
 @Preview
 @Composable
 fun DetailScreenTopAppBarPreview() {
     MusifyTheme {
         DetailScreenTopAppBar(
             title = "Title",
-            onBackButtonClicked = {}
+            onBackButtonClicked = {},
+            dynamicThemeResource = DynamicThemeResource.Empty
         )
     }
 }
